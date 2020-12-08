9e2d81a6d6a1639f60ba32e6e470e25f7c1af1f8
Kyunghwan Kwon <kwon@toanyone.net>
2020-12-08 22:59:08 +0900

    pubsub: refactor

--- src/pubsub.c
+++ src/pubsub.c
@@ -8,7 +8,9 @@
 #include "libmcu/list.h"
 #include "logger.h"
 
-#define TOPIC_DESTROY_MESSAGE			"topic destroyed"
+#if !defined(PUBSUB_TOPIC_DESTROY_MESSAGE)
+#define PUBSUB_TOPIC_DESTROY_MESSAGE		"topic destroyed"
+#endif
 
 typedef struct {
 	char *name;
@@ -28,162 +30,190 @@ static DEFINE_LIST_HEAD(pubsub_list);
 static pthread_mutex_t pubsub_list_lock;
 static volatile bool initialized;
 
-static inline void add_topic(topic_t *p)
+static inline void add_topic(topic_t *topic)
 {
-	pthread_mutex_lock(&pubsub_list_lock);
-	{
-		list_add(&p->pubsub_node, &pubsub_list);
+	list_add(&topic->pubsub_node, &pubsub_list);
+}
+
+static inline void remove_topic(topic_t *topic)
+{
+	list_del(&topic->pubsub_node, &pubsub_list);
+}
+
+static inline void initialize_subscriptions(topic_t *topic)
+{
+	list_init(&topic->subscriptions);
+	pthread_mutex_init(&topic->subscription_lock, NULL);
+}
+
+static inline void remove_subscriptions(topic_t *topic)
+{
+	struct list *i, *j;
+	list_for_each_safe(i, j, &topic->subscriptions) {
+		pubsub_subscribe_t *sub = list_entry(i,
+				pubsub_subscribe_t, subscription_node);
+		list_del(&sub->subscription_node, &topic->subscriptions);
+		free(sub);
 	}
-	pthread_mutex_unlock(&pubsub_list_lock);
 }
 
-static inline void remove_topic(topic_t *p)
-{
-	pthread_mutex_lock(&pubsub_list_lock);
-	{
-		list_del(&p->pubsub_node, &pubsub_list);
-	}
-	pthread_mutex_unlock(&pubsub_list_lock);
-}
-
-static inline void initialize_subscriptions(topic_t *p)
-{
-	list_init(&p->subscriptions);
-	pthread_mutex_init(&p->subscription_lock, NULL);
-}
-
-static inline void remove_subscriptions(topic_t *p)
-{
-	pthread_mutex_lock(&p->subscription_lock);
-	{
-		struct list *i, *j;
-		list_for_each_safe(i, j, &p->subscriptions) {
-			pubsub_subscribe_t *sub = list_entry(i,
-					pubsub_subscribe_t, subscription_node);
-			list_del(&sub->subscription_node, &p->subscriptions);
-			free(sub);
-		}
-	}
-	pthread_mutex_unlock(&p->subscription_lock);
-}
-
-static inline int count_subscribers(topic_t *p)
+static inline int count_subscribers(topic_t *topic)
 {
 	int count = 0;
-
-	pthread_mutex_lock(&p->subscription_lock);
-	{
-		struct list *i;
-		list_for_each(i, &p->subscriptions) {
-			count++;
-		}
+	struct list *i;
+	list_for_each(i, &topic->subscriptions) {
+		count++;
 	}
-	pthread_mutex_unlock(&p->subscription_lock);
-
 	return count;
 }
 
-static topic_t *find_topic(const char * const topic)
+static topic_t *find_topic(const char * const topic_name)
 {
-	topic_t *p = NULL;
+	topic_t *topic = NULL;
 
-	pthread_mutex_lock(&pubsub_list_lock);
-	{
-		struct list *i;
-		list_for_each(i, &pubsub_list) {
-			p = list_entry(i, topic_t, pubsub_node);
-			if (strncmp(topic, p->name, PUBSUB_TOPIC_NAME_MAXLEN)
-					== 0) {
-				break;
-			} else {
-				p = NULL;
-			}
+	struct list *i;
+	list_for_each(i, &pubsub_list) {
+		topic = list_entry(i, topic_t, pubsub_node);
+		if (strncmp(topic_name, topic->name,
+					PUBSUB_TOPIC_NAME_MAXLEN) == 0) {
+			break;
+		} else {
+			topic = NULL;
 		}
 	}
-	pthread_mutex_unlock(&pubsub_list_lock);
 
-	return p;
+	return topic;
 }
 
-pubsub_error_t pubsub_create(const char * const topic)
+static void publish_internal(const topic_t * const topic,
+		const void * const msg, size_t msglen)
 {
+	struct list *i;
+	list_for_each(i, &topic->subscriptions) {
+		pubsub_subscribe_t *sub = list_entry(i,
+				pubsub_subscribe_t, subscription_node);
+		sub->callback(sub->context, msg, msglen);
+	}
+}
+
+pubsub_error_t pubsub_create(const char * const topic_name)
+{
+	pubsub_error_t err = PUBSUB_TOPIC_EXIST;
+	topic_t *new_topic;
 	topic_t *p;
+	size_t topic_len;
 
 	if (!initialized) {
 		pthread_mutex_init(&pubsub_list_lock, NULL);
 		initialized = true;
 	}
 
-	if (!topic) {
+	if (topic_name == NULL) {
 		return PUBSUB_INVALID_PARAM;
 	}
-	if (find_topic(topic)) {
-		return PUBSUB_TOPIC_EXIST;
-	}
-	if (!(p = (topic_t *)calloc(1, sizeof(*p)))) {
+	topic_len = strnlen(topic_name, PUBSUB_TOPIC_NAME_MAXLEN);
+
+	if (!(new_topic = (topic_t *)calloc(1, sizeof(*new_topic)))) {
 		return PUBSUB_NO_MEMORY;
 	}
+	if (!(new_topic->name = (char *)calloc(1, topic_len + 1))) {
+		err = PUBSUB_NO_MEMORY;
+		goto out_free;
+	}
+	strncpy(new_topic->name, topic_name, topic_len);
+	new_topic->name[topic_len] = '\0';
+	initialize_subscriptions(new_topic);
 
-	size_t topic_len = strnlen(topic, PUBSUB_TOPIC_NAME_MAXLEN);
-	if (!(p->name = (char *)calloc(1, topic_len + 1))) {
-		free(p);
-		return PUBSUB_NO_MEMORY;
+	pthread_mutex_lock(&pubsub_list_lock);
+	{
+		if ((p = find_topic(topic_name)) == NULL) {
+			add_topic(new_topic);
+		}
+	}
+	pthread_mutex_unlock(&pubsub_list_lock);
+
+	if (p == NULL) {
+		debug("%s topic created", new_topic->name);
+		return PUBSUB_SUCCESS;
 	}
 
-	strncpy(p->name, topic, topic_len);
-	p->name[topic_len] = '\0';
-	initialize_subscriptions(p);
-	add_topic(p);
-
-	debug("%s topic created", p->name);
-
-	return PUBSUB_SUCCESS;
+	free(new_topic->name);
+out_free:
+	free(new_topic);
+	return err;
 }
 
-pubsub_error_t pubsub_destroy(const char * const topic)
+pubsub_error_t pubsub_destroy(const char * const topic_name)
 {
-	topic_t *p;
-
-	if (!topic) {
+	if (!topic_name) {
 		return PUBSUB_INVALID_PARAM;
 	}
-	if (!(p = find_topic(topic))) {
+
+	topic_t *topic;
+
+	pthread_mutex_lock(&pubsub_list_lock);
+	{
+		if ((topic = find_topic(topic_name)) != NULL) {
+			remove_topic(topic);
+		}
+	}
+	pthread_mutex_unlock(&pubsub_list_lock);
+
+	if (topic == NULL) {
 		return PUBSUB_TOPIC_NOT_EXIST;
 	}
 
-	pubsub_publish(p->name,
-			TOPIC_DESTROY_MESSAGE, sizeof(TOPIC_DESTROY_MESSAGE));
-	remove_subscriptions(p);
-	remove_topic(p);
+	pthread_mutex_lock(&topic->subscription_lock);
+	{
+		publish_internal(topic, PUBSUB_TOPIC_DESTROY_MESSAGE,
+				sizeof(PUBSUB_TOPIC_DESTROY_MESSAGE));
+		remove_subscriptions(topic);
+	}
+	pthread_mutex_unlock(&topic->subscription_lock);
 
-	debug("%s " TOPIC_DESTROY_MESSAGE, p->name);
+	debug("%s " PUBSUB_TOPIC_DESTROY_MESSAGE, topic->name);
 
-	free(p->name);
-	free(p);
+	free(topic->name);
+	free(topic);
 
 	return PUBSUB_SUCCESS;
 }
 
-pubsub_subscribe_t *pubsub_subscribe(const char * const topic,
+pubsub_subscribe_t *pubsub_subscribe(const char * const topic_name,
 		pubsub_callback_t cb, void *context)
 {
-	topic_t *p;
 	pubsub_subscribe_t *sub;
+	topic_t *topic;
 
-	if (!topic || !cb || !(p = find_topic(topic)) ||
-			!(sub = (pubsub_subscribe_t *)calloc(1, sizeof(*sub)))) {
+	if ((topic_name == NULL) || (cb == NULL)) {
+		return NULL;
+	}
+	if ((sub = (pubsub_subscribe_t *)calloc(1, sizeof(*sub))) == NULL) {
 		return NULL;
 	}
 
-	sub->topic = p;
 	sub->callback = cb;
 	sub->context = context;
 
-	pthread_mutex_lock(&p->subscription_lock);
+	pthread_mutex_lock(&pubsub_list_lock);
 	{
-		list_add(&sub->subscription_node, &p->subscriptions);
+		if ((topic = find_topic(topic_name)) != NULL) {
+			sub->topic = topic;
+
+			pthread_mutex_lock(&topic->subscription_lock);
+			{
+				list_add(&sub->subscription_node,
+						&topic->subscriptions);
+			}
+			pthread_mutex_unlock(&topic->subscription_lock);
+		}
 	}
-	pthread_mutex_unlock(&p->subscription_lock);
+	pthread_mutex_unlock(&pubsub_list_lock);
+
+	if (topic == NULL) {
+		free(sub);
+		return NULL;
+	}
 
 	return sub;
 }
@@ -205,44 +235,60 @@ pubsub_error_t pubsub_unsubscribe(pubsub_subscribe_t *sub)
 	return PUBSUB_SUCCESS;
 }
 
-pubsub_error_t pubsub_publish(const char * const topic,
+pubsub_error_t pubsub_publish(const char * const topic_name,
 		const void * const msg, size_t msglen)
 {
-	topic_t *p;
+	topic_t *topic;
 
-	if (!topic || !msg || !msglen) {
+	if (!topic_name || !msg || !msglen) {
 		return PUBSUB_INVALID_PARAM;
 	}
-	if (!(p = find_topic(topic))) {
-		return PUBSUB_TOPIC_NOT_EXIST;
-	}
 
-	pthread_mutex_lock(&p->subscription_lock);
+	pthread_mutex_lock(&pubsub_list_lock);
 	{
-		struct list *i;
-		list_for_each(i, &p->subscriptions) {
-			pubsub_subscribe_t *sub = list_entry(i,
-					pubsub_subscribe_t, subscription_node);
-			sub->callback(sub->context, msg, msglen);
+		if ((topic = find_topic(topic_name)) != NULL) {
+			pthread_mutex_lock(&topic->subscription_lock);
+			{
+				publish_internal(topic, msg, msglen);
+			}
+			pthread_mutex_unlock(&topic->subscription_lock);
 		}
 	}
-	pthread_mutex_unlock(&p->subscription_lock);
+	pthread_mutex_unlock(&pubsub_list_lock);
+
+	if (topic == NULL) {
+		return PUBSUB_TOPIC_NOT_EXIST;
+	}
 
 	return PUBSUB_SUCCESS;
 }
 
-int pubsub_count(const char * const topic)
+int pubsub_count(const char * const topic_name)
 {
-	topic_t *p;
+	topic_t *topic;
+	int count = 0;
 
-	if (!topic) {
+	if (!topic_name) {
 		return PUBSUB_INVALID_PARAM;
 	}
-	if (!(p = find_topic(topic))) {
+
+	pthread_mutex_lock(&pubsub_list_lock);
+	{
+		if ((topic = find_topic(topic_name)) != NULL) {
+			pthread_mutex_lock(&topic->subscription_lock);
+			{
+				count = count_subscribers(topic);
+			}
+			pthread_mutex_unlock(&topic->subscription_lock);
+		}
+	}
+	pthread_mutex_unlock(&pubsub_list_lock);
+
+	if (topic == NULL) {
 		return PUBSUB_TOPIC_NOT_EXIST;
 	}
 
-	return count_subscribers(p);
+	return count;
 }
 
 const char *pubsub_stringify_error(pubsub_error_t err)
@@ -265,6 +311,5 @@ const char *pubsub_stringify_error(pubsub_error_t err)
 	default:
 		break;
 	}
-
 	return "unknown error";
 }
