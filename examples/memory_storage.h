0fe52ff036e12ef6434e6258b991992b4067abaa
Kyunghwan Kwon <kwon@toanyone.net>
2020-12-17 15:36:04 +0900

    example: add memory storage write hook

--- examples/memory_storage.h
+++ examples/memory_storage.h
@@ -1,5 +1,5 @@
-#ifndef LOGGING_MEMORY_STORAGE_H
-#define LOGGING_MEMORY_STORAGE_H
+#ifndef MEMORY_STORAGE_H
+#define MEMORY_STORAGE_H
 
 #if defined(__cplusplus)
 extern "C" {
@@ -10,9 +10,10 @@ extern "C" {
 
 const logging_storage_t *memory_storage_init(void *storage, size_t storage_size);
 void memory_storage_deinit(void);
+void memory_storage_write_hook(const void *data, size_t size);
 
 #if defined(__cplusplus)
 }
 #endif
 
-#endif /* LOGGING_MEMORY_STORAGE_H */
+#endif /* MEMORY_STORAGE_H */
