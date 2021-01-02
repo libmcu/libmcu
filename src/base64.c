c1702a72b5af66b8db95da503d360284ba697dbf
Kyunghwan Kwon <kwon@toanyone.net>
2021-01-03 01:23:41 +0900

    Add base64 (#50)

---
+++ src/base64.c
@@ -0,0 +1,89 @@
+#include "libmcu/base64.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+static char to_base64(uint32_t word, int offset)
+{
+	const char *tbl =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	return tbl[(word >> (offset * 6)) & 0x3fU];
+}
+
+static uint8_t from_base64(char c)
+{
+	if (c >= 'A' && c <= 'Z') {
+		return (uint8_t)(c - 'A');
+	} else if (c >= 'a' && c <= 'z') {
+		return (uint8_t)(c - 'a' + 26);
+	} else if (c >= '0' && c <= '9') {
+		return (uint8_t)(c - '0' + 52);
+	} else if (c == '+') {
+		return 62;
+	} else if (c == '/') {
+		return 63;
+	}
+	return 64;
+}
+
+static uint8_t from_word(uint32_t word, int offset)
+{
+	return (word >> (offset * 8)) & 0xffU;
+}
+
+static size_t decode(uint8_t *out, const char *in, size_t insize)
+{
+	size_t outdex = 0;
+
+	for (size_t i = 0; i < insize; i += 4) {
+		const bool e1 = (in[i+2] == '=');
+		const bool e2 = (in[i+3] == '=');
+
+		const uint32_t b1 = from_base64(in[i]);
+		const uint32_t b2 = from_base64(in[i+1]);
+		const uint32_t b3 = from_base64(in[i+2]);
+		const uint32_t b4 = from_base64(in[i+3]);
+		const uint32_t word = (b1 << 18) | (b2 << 12) | (b3 << 6) | b4;
+
+		out[outdex++] = from_word(word, 2);
+		if (e1) break;
+		out[outdex++] = from_word(word, 1);
+		if (e2) break;
+		out[outdex++] = from_word(word, 0);
+	}
+
+	return outdex;
+}
+
+size_t base64_decode(void *buf, const char *str, size_t strsize)
+{
+	return decode((uint8_t *)buf, str, strsize);
+}
+
+size_t base64_decode_overwrite(char *inout, size_t input_size)
+{
+	return decode((uint8_t *)inout, inout, input_size);
+}
+
+size_t base64_encode(char *buf, const void *data, size_t datasize)
+{
+	const uint8_t *in = (const uint8_t *)data;
+	size_t outdex = 0;
+
+	for (size_t i = 0; i < datasize; i += 3) {
+		const bool c1 = (i+1) < datasize;
+		const bool c2 = (i+2) < datasize;
+
+		const uint32_t b1 = in[i];
+		const uint32_t b2 = c1? in[i+1] : 0;
+		const uint32_t b3 = c2? in[i+2] : 0;
+		const uint32_t word = (b1 << 16) | (b2 << 8) | b3;
+
+		buf[outdex++] = to_base64(word, 3);
+		buf[outdex++] = to_base64(word, 2);
+		buf[outdex++] = c1? to_base64(word, 1) : '=';
+		buf[outdex++] = c2? to_base64(word, 0) : '=';
+	}
+
+	return outdex;
+}
---
+++ tests/src/test_base64.cpp
@@ -0,0 +1,104 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/TestHarness_c.h"
+
+#include <string.h>
+#include "libmcu/base64.h"
+
+TEST_GROUP(base64) {
+	void setup(void) {
+	}
+	void teardown(void) {
+	}
+};
+
+TEST(base64, encode_ShouldReturnLengthOfEncodedData) {
+	const char *fixed = "SGVsbG8sIFdvcmxkIQ==";
+	const char *source = "Hello, World!";
+	char buf[64];
+
+	size_t len = base64_encode(buf, source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+}
+
+TEST(base64, decode_ShouldReturnLengthOfDecodedData) {
+	const char *fixed = "Hello, World!";
+	const char *source = "SGVsbG8sIFdvcmxkIQ==";
+	char buf[64];
+
+	size_t len = base64_decode(buf, source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+}
+
+TEST(base64, decode_overwrite_ShouldReturnLengthOfDecodedData) {
+	const char *fixed = "Hello, World!";
+	char source[] = "SGVsbG8sIFdvcmxkIQ==";
+
+	size_t len = base64_decode_overwrite(source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, source, len);
+}
+
+TEST(base64, encode_decode_a) {
+	const char *fixed = "YQ==";
+	const char *source = "a";
+	char buf[16];
+
+	size_t len = base64_encode(buf, source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+
+	len = base64_decode_overwrite(buf, len);
+	LONGS_EQUAL(strlen(source), len);
+	MEMCMP_EQUAL(source, buf, len);
+}
+
+TEST(base64, encode_decode_ab) {
+	const char *fixed = "YWI=";
+	const char *source = "ab";
+	char buf[16];
+
+	size_t len = base64_encode(buf, source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+
+	len = base64_decode_overwrite(buf, len);
+	LONGS_EQUAL(strlen(source), len);
+	MEMCMP_EQUAL(source, buf, len);
+}
+
+TEST(base64, encode_decode_abc) {
+	const char *fixed = "YWJj";
+	const char *source = "abc";
+	char buf[16];
+
+	size_t len = base64_encode(buf, source, strlen(source));
+
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+
+	len = base64_decode_overwrite(buf, len);
+	LONGS_EQUAL(strlen(source), len);
+	MEMCMP_EQUAL(source, buf, len);
+}
+
+TEST(base64, encode_decode_binary) {
+	const char *fixed = "+EXTRA/TESTa";
+	const uint8_t source[] = {
+		0xf8, 0x45, 0xd3, 0x44, 0x0f, 0xd3, 0x11, 0x24, 0xda, };
+	char buf[16];
+
+	size_t len = base64_encode(buf, source, sizeof(source));
+	LONGS_EQUAL(strlen(fixed), len);
+	MEMCMP_EQUAL(fixed, buf, len);
+
+	len = base64_decode_overwrite(buf, len);
+	LONGS_EQUAL(sizeof(source), len);
+	MEMCMP_EQUAL(source, buf, len);
+}
