d864bcb6f2b194851788fa0fe75f346242ae27d5
Kyunghwan Kwon <kwon@toanyone.net>
2021-03-07 04:35:41 +0900

    Add freertos pthread_mutex port

--- ports/freertos/pthread.c
+++ ports/freertos/pthread.c
@@ -1,4 +1,96 @@
 #include <pthread.h>
+#include <errno.h>
+#include <time.h>
+#include <assert.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+static int pthread_mutex_lock_internal(SemaphoreHandle_t sema, TickType_t timeout)
+{
+	if (xSemaphoreTake(sema, timeout) != pdTRUE) {
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
+{
+	SemaphoreHandle_t sema;
+
+	if (mutex == NULL) {
+		return -EINVAL;
+	}
+	if ((sema = xSemaphoreCreateMutex()) == NULL) {
+		return -EINVAL;
+	}
+
+	*mutex = (pthread_mutex_t)sema;
+
+	return 0;
+}
+
+int pthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+	if (mutex == NULL) {
+		return -EINVAL;
+	}
+
+	SemaphoreHandle_t sema = (SemaphoreHandle_t)*mutex;
+
+	vSemaphoreDelete(sema);
+
+	return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+	if (mutex == NULL) {
+		return -EINVAL;
+	}
+
+	SemaphoreHandle_t sema = (SemaphoreHandle_t)*mutex;
+
+	return pthread_mutex_lock_internal(sema, portMAX_DELAY);
+}
+
+int pthread_mutex_timedlock(pthread_mutex_t *mutex, const struct timespec *timeout)
+{
+	if (mutex == NULL || timeout == NULL) {
+		return -EINVAL;
+	}
+
+	SemaphoreHandle_t sema = (SemaphoreHandle_t)*mutex;
+
+	struct timespec currtime;
+	clock_gettime(CLOCK_REALTIME, &currtime);
+	TickType_t timeout_tick = (timeout->tv_sec - currtime.tv_sec) * 1000 +
+		(timeout->tv_nsec - currtime.tv_nsec) / 1000000;
+
+	if (pthread_mutex_lock_internal(sema, timeout_tick / portTICK_PERIOD_MS) == -EBUSY) {
+		return -ETIMEDOUT;
+	}
+
+	return 0;
+}
+
+int pthread_mutex_trylock(pthread_mutex_t *mutex)
+{
+	return pthread_mutex_lock(mutex);
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	if (mutex == NULL) {
+		return -EINVAL;
+	}
+
+	SemaphoreHandle_t sema = (SemaphoreHandle_t)*mutex;
+
+        int ok = xSemaphoreGive(sema);
+	assert(ok == pdTRUE);
+
+	return 0;
+}
