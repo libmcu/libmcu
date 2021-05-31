a81744d1fa4b291912c36acc22881312f79eb920
Kyunghwan Kwon <kwon@toanyone.net>
2021-05-31 13:31:29 +0900

    add test cases

--- tests/src/button/button_test.cpp
+++ tests/src/button/button_test.cpp
@@ -1,18 +1,31 @@
 #include "CppUTest/TestHarness.h"
+#include "CppUTest/TestHarness_c.h"
+#include "CppUTestExt/MockSupport.h"
+
 #include "libmcu/button.h"
 #include "libmcu/compiler.h"
 
-static unsigned int fake_time;
-static unsigned int fake_state;
-static int pressed_count;
-static int released_count;
-
-static int fake_get_state(void)
+static int get_button_state(void)
 {
-	int ret = (int)(fake_state & 1);
-	fake_state >>= 1;
-	return ret;
+	return mock().actualCall(__func__).returnIntValueOrDefault(0);
 }
+
+static void pressed(const struct button_data *btn, void *context)
+{
+	mock().actualCall(__func__);
+}
+
+static void released(const struct button_data *btn, void *context)
+{
+	mock().actualCall(__func__);
+}
+
+static void holding(const struct button_data *btn, void *context)
+{
+	mock().actualCall(__func__);
+}
+
+static unsigned int fake_time;
 
 static void fake_delayf(unsigned int ms)
 {
@@ -24,72 +37,106 @@ static unsigned int fake_get_time_ms(void)
 	return fake_time++;
 }
 
-static void pressed(const struct button_data *btn, void *context)
-{
-	pressed_count++;
-	unused(btn);
-	unused(context);
-}
-static void released(const struct button_data *btn, void *context)
-{
-	released_count++;
-	unused(btn);
-	unused(context);
-}
-
 TEST_GROUP(button) {
 	struct button_handlers handlers;
 
 	void setup(void) {
+		mock().ignoreOtherCalls();
+
 		button_init(fake_get_time_ms, fake_delayf);
 		handlers.pressed = pressed;
 		handlers.released = released;
+		handlers.holding = holding;
 		fake_time = 0;
-		fake_state = 0;
-		pressed_count = 0;
-		released_count = 0;
 	}
 	void teardown(void) {
+		mock().checkExpectations();
+		mock().clear();
 	}
 };
 
 TEST(button, register_ShouldReturnFalse_WhenInvalidParamsGiven) {
 	LONGS_EQUAL(0, button_register(NULL, NULL));
 	LONGS_EQUAL(0, button_register(&handlers, NULL));
-	LONGS_EQUAL(0, button_register(NULL, fake_get_state));
+	LONGS_EQUAL(0, button_register(NULL, get_button_state));
 }
 
 TEST(button, register_ShouldReturnFalse_WhenNoSlotsLeft) {
-	LONGS_EQUAL(1, button_register(&handlers, fake_get_state));
-	LONGS_EQUAL(1, button_register(&handlers, fake_get_state));
-	LONGS_EQUAL(1, button_register(&handlers, fake_get_state));
-	LONGS_EQUAL(0, button_register(&handlers, fake_get_state));
+	LONGS_EQUAL(1, button_register(&handlers, get_button_state));
+	LONGS_EQUAL(1, button_register(&handlers, get_button_state));
+	LONGS_EQUAL(1, button_register(&handlers, get_button_state));
+	LONGS_EQUAL(0, button_register(&handlers, get_button_state));
 }
 
 TEST(button, register_ShouldReturnTrue) {
-	LONGS_EQUAL(1, button_register(&handlers, fake_get_state));
+	LONGS_EQUAL(1, button_register(&handlers, get_button_state));
 }
 
 TEST(button, poll_ShouldDoNothing_WhenNoiseGiven) {
-	fake_state = 0x1cf1f; // 0b1_1100_1111_0001_1111
-	button_register(&handlers, fake_get_state);
+	//0x1cf1f: 0b1_1100_1111_0001_1111
+	mock().expectNCalls(3, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(2, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(4, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(3, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(5, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(7, "get_button_state").andReturnValue(0);
+
+	button_register(&handlers, get_button_state);
 	button_poll(NULL);
-	LONGS_EQUAL(0, pressed_count);
-	LONGS_EQUAL(0, released_count);
+
+	mock().expectNoCall("pressed");
+	mock().expectNoCall("holding");
+	mock().expectNoCall("released");
 }
 
 TEST(button, poll_ShouldCallPressed_WhenValidPatternGiven) {
-	fake_state = 0x35fff; // 0b0011_0101_1111_1111_1111
-	button_register(&handlers, fake_get_state);
+	//0x35fff: 0b11_0101_1111_1111_1111
+	mock().expectNCalls(2, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(13, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(7, "get_button_state").andReturnValue(0);
+
+	mock().expectOneCall("pressed");
+	mock().expectNoCall("holding");
+	mock().expectOneCall("released");
+
+	button_register(&handlers, get_button_state);
 	button_poll(NULL);
-	LONGS_EQUAL(1, pressed_count);
-	LONGS_EQUAL(1, released_count);
 }
 
 TEST(button, poll_ShouldIgnoreNoise_WhenNoiseGivenInMiddleOfPressed) {
-	fake_state = 0xffc35fff; // 0b1111_1111_1100_0011_0101_1111_1111_1111
-	button_register(&handlers, fake_get_state);
+	//0xffc35fff: 0b1111_1111_1100_0011_0101_1111_1111_1111
+	mock().expectNCalls(10, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(4, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(2, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(13, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(7, "get_button_state").andReturnValue(0);
+
+	mock().expectOneCall("pressed");
+	mock().expectNoCall("holding");
+	mock().expectOneCall("released");
+
+	button_register(&handlers, get_button_state);
 	button_poll(NULL);
-	LONGS_EQUAL(1, pressed_count);
-	LONGS_EQUAL(1, released_count);
+}
+
+TEST(button, poll_ShouldCallHolding_WhenHoldingButtonPressed) {
+	mock().expectNCalls(2, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(1, "get_button_state").andReturnValue(0);
+	mock().expectNCalls(35, "get_button_state").andReturnValue(1);
+	mock().expectNCalls(7, "get_button_state").andReturnValue(0);
+
+	mock().expectOneCall("pressed");
+	mock().expectOneCall("holding");
+	mock().expectOneCall("released");
+
+	button_register(&handlers, get_button_state);
+	button_poll(NULL);
 }
