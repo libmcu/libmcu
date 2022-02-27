eff9547e301476a6d16c61ad95b1b7e28b82d25c
Kyunghwan Kwon <kwon@toanyone.net>
2022-02-27 16:16:51 +0900

    common: add COBS

---
+++ tests/runners/common/cobs.mk
@@ -0,0 +1,17 @@
+COMPONENT_NAME = cobs
+
+SRC_FILES = \
+	../modules/common/src/cobs.c
+
+TEST_SRC_FILES = \
+	src/common/cobs_test.cpp \
+	src/test_all.cpp \
+
+INCLUDE_DIRS = \
+	../modules/common/include \
+	$(CPPUTEST_HOME)/include \
+
+MOCKS_SRC_DIRS =
+CPPUTEST_CPPFLAGS =
+
+include MakefileRunner.mk
