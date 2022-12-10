bd6054196e476a092b4645118618549995eebb00
Kyunghwan Kwon <k@mononn.com>
2022-12-10 15:24:03 +0900

    fix build error

--- modules/ao/src/ao_overrides.c
+++ modules/ao/src/ao_overrides.c
@@ -5,17 +5,18 @@
 LIBMCU_WEAK
 void ao_lock(void *lock_handle)
 {
-	pthread_mutex_lock(lock_handle);
+	pthread_mutex_lock((pthread_mutex_t *)lock_handle);
 }
 
 LIBMCU_WEAK
 void ao_unlock(void *lock_handle)
 {
-	pthread_mutex_unlock(lock_handle);
+	pthread_mutex_unlock((pthread_mutex_t *)lock_handle);
 }
 
 LIBMCU_WEAK
 int ao_lock_init(void *lock_handle, void *arg)
 {
-	return pthread_mutex_init(lock_handle, arg);
+	return pthread_mutex_init((pthread_mutex_t *)lock_handle,
+			(const pthread_mutexattr_t *)arg);
 }
