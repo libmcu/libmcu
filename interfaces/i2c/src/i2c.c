338ef14260161a4bc23246cb107cc3bdd9b0d68c
Kyunghwan Kwon <k@mononn.com>
2023-04-14 14:09:45 +0900

    Add an interface for UART (#127)

--- interfaces/i2c/src/i2c.c
+++ interfaces/i2c/src/i2c.c
@@ -1,14 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 #include "libmcu/i2c.h"
 #include "libmcu/port/i2c.h"
 
-struct i2c *i2c_create(uint8_t id)
+struct i2c *i2c_create(uint8_t channel)
 {
-	return i2c_port_create(id);
+	return i2c_port_create(channel);
 }
 
 void i2c_delete(struct i2c *self)
 {
 	i2c_port_delete(self);
+}
+
+int i2c_enable(struct i2c *self)
+{
+	return i2c_port_enable(self);
+}
+
+int i2c_disable(struct i2c *self)
+{
+	return i2c_port_disable(self);
 }
 
 int i2c_write(struct i2c *self, uint8_t addr, uint8_t reg,
