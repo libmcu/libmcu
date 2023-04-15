07b241d8b282ee21ccb0483917ef7689984ccc16
Kyunghwan Kwon <k@mononn.com>
2023-04-15 10:05:48 +0900

    Add an interface for ADC

---
+++ interfaces/adc/include/libmcu/adc.h
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef LIBMCU_ADC_H
+#define LIBMCU_ADC_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+struct adc;
+
+struct adc *adc_create(uint8_t adc_num);
+int adc_delete(struct adc *self);
+int adc_enable(struct adc *self);
+int adc_disable(struct adc *self);
+int adc_channel_init(struct adc *self, uint16_t channel);
+int adc_calibrate(struct adc *self);
+int adc_measure(struct adc *self);
+int adc_convert_to_millivolts(struct adc *self, int value);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* LIBMCU_ADC_H */
