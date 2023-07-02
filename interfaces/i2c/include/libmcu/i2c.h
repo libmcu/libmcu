ab4a0281a5fcde01a4d3ae047974f823642c20d5
Kyunghwan Kwon <k@mononn.com>
2023-07-02 16:54:29 +0900

    interfaces: i2c: change read/write prototype (#137)

--- interfaces/i2c/include/libmcu/i2c.h
+++ interfaces/i2c/include/libmcu/i2c.h
@@ -13,17 +13,25 @@ extern "C" {
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-struct i2c;
+struct i2c {
+	bool enabled;
+};
 
 struct i2c *i2c_create(uint8_t channel);
 void i2c_delete(struct i2c *self);
 int i2c_enable(struct i2c *self);
 int i2c_disable(struct i2c *self);
-int i2c_write(struct i2c *self, uint8_t addr, uint8_t reg,
+int i2c_read(struct i2c *self, uint8_t slave_addr, void *buf, size_t bufsize);
+int i2c_write(struct i2c *self, uint8_t slave_addr,
 		const void *data, size_t data_len);
-int i2c_read(struct i2c *self, uint8_t addr, uint8_t reg,
+int i2c_read_reg(struct i2c *self, uint8_t slave_addr,
+		uint32_t reg_addr, uint8_t reg_addr_bits,
 		void *buf, size_t bufsize);
+int i2c_write_reg(struct i2c *self, uint8_t slave_addr,
+		uint32_t reg_addr, uint8_t reg_addr_bits,
+		const void *data, size_t data_len);
 
 #if defined(__cplusplus)
 }
--- interfaces/i2c/include/libmcu/port/i2c.h
+++ interfaces/i2c/include/libmcu/port/i2c.h
@@ -17,10 +17,16 @@ struct i2c *i2c_port_create(uint8_t channel);
 void i2c_port_delete(struct i2c *self);
 int i2c_port_enable(struct i2c *self);
 int i2c_port_disable(struct i2c *self);
-int i2c_port_write(struct i2c *self, uint8_t addr, uint8_t reg,
-		const void *data, size_t data_len);
-int i2c_port_read(struct i2c *self, uint8_t addr, uint8_t reg,
+int i2c_port_read(struct i2c *self, uint8_t slave_addr,
 		void *buf, size_t bufsize);
+int i2c_port_write(struct i2c *self, uint8_t slave_addr,
+		const void *data, size_t data_len);
+int i2c_port_read_reg(struct i2c *self, uint8_t slave_addr,
+		uint32_t reg_addr, uint8_t reg_addr_bits,
+		void *buf, size_t bufsize);
+int i2c_port_write_reg(struct i2c *self, uint8_t slave_addr,
+		uint32_t reg_addr, uint8_t reg_addr_bits,
+		const void *data, size_t data_len);
 
 #if defined(__cplusplus)
 }
