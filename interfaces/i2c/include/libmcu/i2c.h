8db386eb6beb7b1de6df34a675af6a25d993158a
Kyunghwan Kwon <k@mononn.com>
2023-07-05 04:34:54 +0900

    interfaces: i2c: add timeout parameter

--- interfaces/i2c/include/libmcu/i2c.h
+++ interfaces/i2c/include/libmcu/i2c.h
@@ -23,15 +23,16 @@ struct i2c *i2c_create(uint8_t channel);
 void i2c_delete(struct i2c *self);
 int i2c_enable(struct i2c *self);
 int i2c_disable(struct i2c *self);
-int i2c_read(struct i2c *self, uint8_t slave_addr, void *buf, size_t bufsize);
+int i2c_read(struct i2c *self, uint8_t slave_addr,
+		void *buf, size_t bufsize, uint32_t timeout_ms);
 int i2c_write(struct i2c *self, uint8_t slave_addr,
-		const void *data, size_t data_len);
+		const void *data, size_t data_len, uint32_t timeout_ms);
 int i2c_read_reg(struct i2c *self, uint8_t slave_addr,
 		uint32_t reg_addr, uint8_t reg_addr_bits,
-		void *buf, size_t bufsize);
+		void *buf, size_t bufsize, uint32_t timeout_ms);
 int i2c_write_reg(struct i2c *self, uint8_t slave_addr,
 		uint32_t reg_addr, uint8_t reg_addr_bits,
-		const void *data, size_t data_len);
+		const void *data, size_t data_len, uint32_t timeout_ms);
 
 #if defined(__cplusplus)
 }
--- interfaces/i2c/src/i2c.c
+++ interfaces/i2c/src/i2c.c
@@ -46,45 +46,46 @@ int i2c_disable(struct i2c *self)
 	return err;
 }
 
-int i2c_read(struct i2c *self, uint8_t slave_addr, void *buf, size_t bufsize)
+int i2c_read(struct i2c *self, uint8_t slave_addr,
+		void *buf, size_t bufsize, uint32_t timeout_ms)
 {
 	if (!self->enabled) {
 		return -ENODEV;
 	}
 
-	return i2c_port_read(self, slave_addr, buf, bufsize);
+	return i2c_port_read(self, slave_addr, buf, bufsize, timeout_ms);
 }
 
 int i2c_write(struct i2c *self, uint8_t slave_addr,
-		const void *data, size_t data_len)
+		const void *data, size_t data_len, uint32_t timeout_ms)
 {
 	if (!self->enabled) {
 		return -ENODEV;
 	}
 
-	return i2c_port_write(self, slave_addr, data, data_len);
+	return i2c_port_write(self, slave_addr, data, data_len, timeout_ms);
 }
 
 int i2c_read_reg(struct i2c *self, uint8_t slave_addr,
 		uint32_t reg_addr, uint8_t reg_addr_bits,
-		void *buf, size_t bufsize)
+		void *buf, size_t bufsize, uint32_t timeout_ms)
 {
 	if (!self->enabled) {
 		return -ENODEV;
 	}
 
 	return i2c_port_read_reg(self, slave_addr, reg_addr, reg_addr_bits,
-			buf, bufsize);
+			buf, bufsize, timeout_ms);
 }
 
 int i2c_write_reg(struct i2c *self, uint8_t slave_addr,
 		uint32_t reg_addr, uint8_t reg_addr_bits,
-		const void *data, size_t data_len)
+		const void *data, size_t data_len, uint32_t timeout_ms)
 {
 	if (!self->enabled) {
 		return -ENODEV;
 	}
 
 	return i2c_port_write_reg(self, slave_addr, reg_addr, reg_addr_bits,
-			data, data_len);
+			data, data_len, timeout_ms);
 }
