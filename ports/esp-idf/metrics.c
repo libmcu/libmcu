5bf5bcec8d886fa99de991d7f4dea584812ca827
Kyunghwan Kwon <k@mononn.com>
2023-07-10 22:03:43 +0900

    port: esp-idf: separate encoder

--- ports/esp-idf/metrics.c
+++ ports/esp-idf/metrics.c
@@ -9,10 +9,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "cbor/cbor.h"
-#include "cbor/encoder.h"
-
-static cbor_writer_t writer;
 static portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED;
 
 void metrics_lock(void)
@@ -23,33 +19,4 @@ void metrics_lock(void)
 void metrics_unlock(void)
 {
 	taskEXIT_CRITICAL(&spinlock);
-}
-
-size_t metrics_encode_header(void *buf, size_t bufsize,
-		uint32_t nr_total, uint32_t nr_updated)
-{
-	cbor_writer_init(&writer, buf, bufsize);
-	cbor_encode_map(&writer, nr_updated);
-
-	return cbor_writer_len(&writer);
-}
-
-size_t metrics_encode_each(void *buf, size_t bufsize,
-		metric_key_t key, int32_t value)
-{
-	if (value == 0) {
-		return 0;
-	}
-
-	size_t len = cbor_writer_len(&writer);
-
-	cbor_encode_unsigned_integer(&writer, (uint64_t)key);
-
-	if (value >= 0) {
-		cbor_encode_unsigned_integer(&writer, (uint64_t)value);
-	} else {
-		cbor_encode_negative_integer(&writer, value);
-	}
-
-	return cbor_writer_len(&writer) - len;
 }
--- ports/posix/metrics.c
+++ ports/posix/metrics.c
@@ -6,10 +6,7 @@
 
 #include "libmcu/metrics.h"
 #include <pthread.h>
-#include "cbor/cbor.h"
-#include "cbor/encoder.h"
 
-static cbor_writer_t writer;
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void metrics_lock(void)
@@ -20,33 +17,4 @@ void metrics_lock(void)
 void metrics_unlock(void)
 {
 	pthread_mutex_unlock(&lock);
-}
-
-size_t metrics_encode_header(void *buf, size_t bufsize,
-		uint32_t nr_total, uint32_t nr_updated)
-{
-	cbor_writer_init(&writer, buf, bufsize);
-	cbor_encode_map(&writer, nr_updated);
-
-	return cbor_writer_len(&writer);
-}
-
-size_t metrics_encode_each(void *buf, size_t bufsize,
-		metric_key_t key, int32_t value)
-{
-	if (value == 0) {
-		return 0;
-	}
-
-	size_t len = cbor_writer_len(&writer);
-
-	cbor_encode_unsigned_integer(&writer, (uint64_t)key);
-
-	if (value >= 0) {
-		cbor_encode_unsigned_integer(&writer, (uint64_t)value);
-	} else {
-		cbor_encode_negative_integer(&writer, value);
-	}
-
-	return cbor_writer_len(&writer) - len;
 }
