d26f187dd62babfdbf92216534df028eb9a11361
Kyunghwan Kwon <k@mononn.com>
2023-08-06 17:08:54 +0900

    add callback on wifi mock

--- tests/mocks/wifi.cpp
+++ tests/mocks/wifi.cpp
@@ -1,7 +1,12 @@
 #include "CppUTestExt/MockSupport.h"
 #include "libmcu/wifi.h"
 
+static struct wifi *iface;
+static wifi_event_callback_t callback;
+static void *callback_ctx;
+
 int wifi_enable(struct wifi *self) {
+	iface = self;
 	return mock().actualCall(__func__).returnIntValue();
 }
 
@@ -10,7 +15,11 @@ int wifi_disable(struct wifi *self) {
 }
 
 int wifi_connect(struct wifi *self, const struct wifi_conn_param *param) {
-	return mock().actualCall(__func__).returnIntValue();
+	int rc = mock().actualCall(__func__).returnIntValue();
+	if (callback) {
+		(*callback)(iface, WIFI_EVT_CONNECTED, 0, callback_ctx);
+	}
+	return rc;
 }
 
 int wifi_disconnect(struct wifi *self) {
@@ -22,5 +31,7 @@ int wifi_scan(struct wifi *self) {
 }
 
 int wifi_register_event_callback(struct wifi *self, enum wifi_event event_type, const wifi_event_callback_t cb, void *user_ctx) {
+	callback = cb;
+	callback_ctx = user_ctx;
 	return mock().actualCall(__func__).returnIntValue();
 }
