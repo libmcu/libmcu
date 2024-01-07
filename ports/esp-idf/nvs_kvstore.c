5a35d624e6cc5bd6818198f6d4ed42b3bfbb9586
Kyunghwan Kwon <k@mononn.com>
2024-01-07 14:58:30 +0900

    fix nvs_kvstore ports

--- ports/esp-idf/nvs_kvstore.c
+++ ports/esp-idf/nvs_kvstore.c
@@ -16,8 +16,8 @@
 
 #include "libmcu/assert.h"
 
-struct nvs_kvstore {
-	struct kvstore super;
+struct kvstore {
+	struct kvstore_api api;
 	nvs_handle_t handle;
 };
 
@@ -33,52 +33,45 @@ static void nvs_storage_close(nvs_handle_t *namespace_handle)
 
 static int nvs_kvstore_write(struct kvstore *self, char const *key, void const *value, size_t size)
 {
-	struct nvs_kvstore *nvs_kvstore = (struct nvs_kvstore *)self;
-
-	int err = nvs_set_blob(nvs_kvstore->handle, key, value, size);
+	int err = nvs_set_blob(self->handle, key, value, size);
 	if (err) {
 		return 0;
 	}
-	return !nvs_commit(nvs_kvstore->handle)? size : 0;
+	return !nvs_commit(self->handle)? size : 0;
 }
 
 static int nvs_kvstore_read(struct kvstore *self, char const *key, void *buf, size_t size)
 {
-	struct nvs_kvstore *nvs_kvstore = (struct nvs_kvstore *)self;
-	return !nvs_get_blob(nvs_kvstore->handle, key, buf, &size)? size : 0;
+	return !nvs_get_blob(self->handle, key, buf, &size)? size : 0;
 }
 
 static int nvs_kvstore_erase(struct kvstore *self, char const *key)
 {
-	struct nvs_kvstore *nvs_kvstore = (struct nvs_kvstore *)self;
-	return nvs_erase_key(nvs_kvstore->handle, key);
+	return nvs_erase_key(self->handle, key);
 }
 
 static int nvs_kvstore_open(struct kvstore *self, char const *namespace)
 {
-	struct nvs_kvstore *nvs_kvstore = (struct nvs_kvstore *)self;
-	return nvs_storage_open(namespace, &nvs_kvstore->handle);
+	return nvs_storage_open(namespace, &self->handle);
 }
 
 static void nvs_kvstore_close(struct kvstore *self)
 {
-	struct nvs_kvstore *nvs_kvstore = (struct nvs_kvstore *)self;
-	nvs_storage_close(&nvs_kvstore->handle);
+	nvs_storage_close(&self->handle);
 }
 
 /* NOTE: nvs should be initialized first before calling the function. Call
  * `nvs_flash_init()` first to initialize. */
 struct kvstore *nvs_kvstore_new(void)
 {
-	struct nvs_kvstore *nvs_kvstore = malloc(sizeof(*nvs_kvstore));
+	struct kvstore *nvs_kvstore = malloc(sizeof(*nvs_kvstore));
 	assert(nvs_kvstore);
 
-	struct kvstore *kvstore = (struct kvstore *)nvs_kvstore;
-	kvstore->write = nvs_kvstore_write;
-	kvstore->read = nvs_kvstore_read;
-	kvstore->clear = nvs_kvstore_erase;
-	kvstore->open = nvs_kvstore_open;
-	kvstore->close = nvs_kvstore_close;
+	nvs_kvstore->api.write = nvs_kvstore_write;
+	nvs_kvstore->api.read = nvs_kvstore_read;
+	nvs_kvstore->api.clear = nvs_kvstore_erase;
+	nvs_kvstore->api.open = nvs_kvstore_open;
+	nvs_kvstore->api.close = nvs_kvstore_close;
 
-	return kvstore;
+	return nvs_kvstore;
 }
