3110b94a775758283428ea9e16532331d47475bd
Kyunghwan Kwon <k@mononn.com>
2024-01-09 16:27:55 +0900

    add reclaim

--- ports/kvstore/flash_kvstore.c
+++ ports/kvstore/flash_kvstore.c
@@ -12,6 +12,11 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <errno.h>
+#include <string.h>
+
+#if !defined(FLASH_LINE_ALIGN_BYTES)
+#define FLASH_LINE_ALIGN_BYTES		16
+#endif
 
 #if !defined(MIN)
 #define MIN(a, b)			((a) > (b)? (b) : (a))
@@ -22,7 +27,9 @@ struct meta_entry {
 	uint32_t hash_dbj2;
 	uint32_t offset;
 	uint32_t len;
+	uint8_t _padding[FLASH_LINE_ALIGN_BYTES - 16];
 } LIBMCU_PACKED;
+LIBMCU_ASSERT(sizeof(struct meta_entry) == FLASH_LINE_ALIGN_BYTES);
 
 struct meta {
 	struct meta_entry entry;
@@ -35,19 +42,20 @@ struct subsector {
 };
 
 struct storage {
+	struct flash *flash;
+
 	struct subsector meta;
 	struct subsector data;
 
+	uintptr_t offset;
 	size_t size;
-
-	struct flash *flash;
-	struct flash *flash_scratch;
 };
 
 struct kvstore {
 	struct kvstore_api api;
 
 	struct storage storage;
+	struct storage scratch;
 };
 
 static bool is_free_entry(struct meta_entry *entry)
@@ -60,41 +68,81 @@ static bool is_free_entry(struct meta_entry *entry)
 	return false;
 }
 
-static int find_key(struct kvstore *self, const char *key, struct meta *meta)
+static int find_key(struct storage *storage, const char *key, struct meta *meta)
 {
 	uint32_t hash_murmur = hash_murmur_32(key);
 	uint32_t hash_dbj2 = hash_dbj2_32(key);
 
-	uintptr_t start = self->storage.meta.offset;
-	uintptr_t end = start + self->storage.meta.size;
+	uintptr_t start = storage->offset + storage->meta.offset;
+	uintptr_t end = start + storage->meta.size;
 	uint32_t entry_size = sizeof(struct meta_entry);
+	int keycnt = 0;
 
 	for (uintptr_t offset = start; offset < end; offset += entry_size) {
-		if (flash_read(self->storage.flash,
-				offset, &meta->entry, entry_size) < 0) {
+		struct meta t;
+
+		if (flash_read(storage->flash,
+				offset, &t.entry, entry_size) < 0) {
 			return -EIO;
 		}
 
-		if (hash_murmur == meta->entry.hash_murmur &&
-				hash_dbj2 == meta->entry.hash_dbj2) {
+		/* the last one is the latest one */
+		if (hash_murmur == t.entry.hash_murmur &&
+				hash_dbj2 == t.entry.hash_dbj2) {
+			memcpy(&meta->entry, &t.entry, sizeof(t.entry));
 			meta->offset = offset;
-			return 0;
+			keycnt++;
 		}
+	}
+
+	if (keycnt > 0) {
+		return 0;
 	}
 
 	return -ENOENT;
 }
 
-static int alloc_entry(struct kvstore *self, size_t size, struct meta *meta)
+static int find_meta(struct storage *storage, struct meta *meta)
 {
-	uintptr_t start = self->storage.meta.offset;
-	uintptr_t end = start + self->storage.meta.size;
+	uintptr_t start = storage->offset + storage->meta.offset;
+	uintptr_t end = start + storage->meta.size;
+	uint32_t entry_size = sizeof(struct meta_entry);
+	int keycnt = 0;
+
+	for (uintptr_t offset = start; offset < end; offset += entry_size) {
+		struct meta t;
+
+		if (flash_read(storage->flash,
+				offset, &t.entry, entry_size) < 0) {
+			return -EIO;
+		}
+
+		/* the last one is the latest one */
+		if (meta->entry.hash_murmur == t.entry.hash_murmur &&
+				meta->entry.hash_dbj2 == t.entry.hash_dbj2) {
+			memcpy(&meta->entry, &t.entry, sizeof(t.entry));
+			meta->offset = offset;
+			keycnt++;
+		}
+	}
+
+	if (keycnt > 0) {
+		return 0;
+	}
+
+	return -ENOENT;
+}
+
+static int alloc_entry(struct storage *storage, size_t size, struct meta *meta)
+{
+	uintptr_t start = storage->offset + storage->meta.offset;
+	uintptr_t end = start + storage->meta.size;
 	uint32_t entry_size = sizeof(struct meta_entry);
 	uint32_t new_data_offset = 0;
 	bool allocated = false;
 
 	for (uintptr_t offset = start; offset < end; offset += entry_size) {
-		if (flash_read(self->storage.flash,
+		if (flash_read(storage->flash,
 				offset, &meta->entry, entry_size) < 0) {
 			return -EIO;
 		}
@@ -103,14 +151,16 @@ static int alloc_entry(struct kvstore *self, size_t size, struct meta *meta)
 			meta->offset = offset;
 			allocated = true;
 		} else {
-			uint32_t t = meta->entry.offset + meta->entry.len;
-			if (t > new_data_offset && t < self->storage.data.size) {
+			uint32_t t = ALIGN(meta->entry.offset + meta->entry.len,
+					FLASH_LINE_ALIGN_BYTES);
+			if (t > new_data_offset &&
+					t < storage->data.size) {
 				new_data_offset = t;
 			}
 		}
 	}
 
-	if (allocated && (new_data_offset + size) < self->storage.data.size) {
+	if (allocated && (new_data_offset + size) < storage->data.size) {
 		meta->entry.offset = new_data_offset;
 		meta->entry.len = (uint32_t)size;
 		return 0;
@@ -119,41 +169,107 @@ static int alloc_entry(struct kvstore *self, size_t size, struct meta *meta)
 	return -ENOSPC;
 }
 
-static int delete_meta(struct kvstore *self, struct meta *meta)
+#if defined(FLASH_OVERWRITE)
+static int delete_meta(struct storage *storage, struct meta *meta)
 {
 	struct meta_entry *entry = &meta->entry;
 
 	entry->hash_murmur = 0;
 	entry->hash_dbj2 = 0;
 
-	return flash_write(self->storage.flash,
+	return flash_write(storage->flash,
 			meta->offset, entry, sizeof(*entry));
 }
+#endif
 
-static int write_meta(struct kvstore *self, const struct meta *meta)
+static int write_meta(struct storage *storage, const struct meta *meta)
 {
-	return flash_write(self->storage.flash,
+	return flash_write(storage->flash,
 			meta->offset, &meta->entry, sizeof(meta->entry));
 }
 
-static int write_value(struct kvstore *self,
+static int write_value(struct storage *storage,
 		const void *data, const struct meta *meta)
 {
-	uintptr_t offset = self->storage.meta.size + meta->entry.offset;
-	return flash_write(self->storage.flash, offset, data, meta->entry.len);
+	uintptr_t offset = storage->offset +
+		storage->meta.offset + storage->meta.size +
+		meta->entry.offset;
+	return flash_write(storage->flash, offset, data, meta->entry.len);
 }
 
-/* TODO: implement get_available_bytes() */
-static size_t get_available_bytes(struct kvstore *self)
-{
-	unused(self);
-	return 0;
-}
+/* It should keep at least one validate partition even if an error occurs in the
+ * middle of flash processing. */
+static int reclaim(struct kvstore *self) {
+        struct storage *p = &self->storage;
+	struct storage *t = &self->scratch;
 
-/* TODO: implement reclaim() */
-static int reclaim(struct kvstore *self)
-{
-	unused(self);
+	if (!t->flash) {
+		return -ENOTSUP;
+	}
+
+	uintptr_t start = p->offset + p->meta.offset;
+	uintptr_t end = start + p->meta.size;
+	uint32_t entry_size = sizeof(struct meta_entry);
+	struct meta meta;
+
+	flash_erase(t->flash, t->offset, t->size);
+
+	for (uintptr_t offset = start; offset < end; offset += entry_size) {
+		struct meta new_meta;
+
+		if (flash_read(p->flash, offset, &meta.entry, entry_size) < 0) {
+			return -EIO;
+		}
+
+		if (is_free_entry(&meta.entry) ||
+				find_meta(p, &meta) != 0 ||
+				find_meta(t, &meta) == 0) {
+			continue;
+		}
+
+		if (alloc_entry(t, meta.entry.len, &new_meta) < 0) {
+			return -EIO;
+		}
+
+		new_meta.entry.hash_murmur = meta.entry.hash_murmur;
+		new_meta.entry.hash_dbj2 = meta.entry.hash_dbj2;
+
+		if (write_meta(t, &new_meta) < 0) {
+			return -EIO;
+		}
+
+		for (uint32_t i = 0; i < new_meta.entry.len; i++) {
+			uint8_t buf[FLASH_LINE_ALIGN_BYTES];
+                        uintptr_t ofs_f = p->offset + p->meta.offset +
+				p->meta.size + meta.entry.offset + i;
+                        uintptr_t ofs_t = t->offset + t->meta.offset +
+				t->meta.size + new_meta.entry.offset + i;
+                        if (flash_read(p->flash, ofs_f, buf, sizeof(buf)) < 0) {
+                                return -EIO;
+                        }
+                        if (flash_write(t->flash, ofs_t, buf, sizeof(buf)) < 0) {
+				return -EIO;
+			}
+		}
+	}
+
+	if (alloc_entry(t, FLASH_LINE_ALIGN_BYTES, &meta) == -ENOSPC) {
+		return -ENOSPC;
+	}
+
+	flash_erase(p->flash, p->offset, p->size);
+
+	for (uintptr_t offset = t->offset; offset < t->size;
+			offset += FLASH_LINE_ALIGN_BYTES) {
+		uint8_t buf[FLASH_LINE_ALIGN_BYTES];
+		if (flash_read(t->flash, offset, buf, sizeof(buf)) < 0) {
+			return -EIO;
+		}
+		if (flash_write(p->flash, offset, buf, sizeof(buf)) < 0) {
+			return -EIO;
+		}
+	}
+
 	return 0;
 }
 
@@ -161,28 +277,30 @@ static int flash_kvstore_write(struct kvstore *self,
 		const char *key, const void *value, size_t size)
 {
 	struct meta new_meta;
+	int rc;
+
+	if ((rc = alloc_entry(&self->storage, size, &new_meta)) != 0) {
+		if (rc == -ENOSPC && (rc = reclaim(self)) == 0) {
+			rc = alloc_entry(&self->storage, size, &new_meta);
+		}
+
+		if (rc != 0) {
+			return rc;
+		}
+	}
+
+#if defined(FLASH_OVERWRITE)
 	struct meta old_meta;
-
-	if (alloc_entry(self, size, &new_meta) != 0) {
-		if (get_available_bytes(self) < size) {
-			return -ENOSPC;
-		}
-
-		if (reclaim(self) != 0 ||
-				alloc_entry(self, size, &new_meta) != 0) {
-			return -EFAULT;
-		}
+	if (find_key(&self->storage, key, &old_meta) == 0) {
+		delete_meta(&self->storage, &old_meta);
 	}
-
-	if (find_key(self, key, &old_meta) == 0) {
-		delete_meta(self, &old_meta);
-	}
+#endif
 
 	new_meta.entry.hash_murmur = hash_murmur_32(key);
 	new_meta.entry.hash_dbj2 = hash_dbj2_32(key);
 
-	if (write_meta(self, &new_meta) != 0 ||
-			write_value(self, value, &new_meta) != 0) {
+	if (write_meta(&self->storage, &new_meta) != 0 ||
+			write_value(&self->storage, value, &new_meta) != 0) {
 		return -EIO;
 	}
 
@@ -194,24 +312,33 @@ static int flash_kvstore_read(struct kvstore *self,
 {
 	struct meta meta;
 
-	if (find_key(self, key, &meta) != 0) {
+	if (find_key(&self->storage, key, &meta) != 0) {
 		return -ENOENT;
 	}
 
 	size_t len = MIN(size, meta.entry.len);
-	uintptr_t offset = self->storage.meta.size + meta.entry.offset;
+	uintptr_t offset = self->storage.offset +
+		self->storage.meta.offset + self->storage.meta.size +
+		meta.entry.offset;
 	return flash_read(self->storage.flash, offset, buf, len);
 }
 
 static int flash_kvstore_erase(struct kvstore *self, const char *key)
 {
+#if defined(FLASH_OVERWRITE)
 	struct meta meta;
 
-	if (find_key(self, key, &meta) == 0) {
-		return delete_meta(self, &meta);
+	if (find_key(&self->storage, key, &meta) == 0) {
+		return delete_meta(&self->storage, &meta);
 	}
 
 	return -ENOENT;
+#else
+	unused(self);
+	unused(key);
+
+	return -ENOTSUP;
+#endif
 }
 
 static int flash_kvstore_open(struct kvstore *self, const char *namespace)
@@ -219,7 +346,7 @@ static int flash_kvstore_open(struct kvstore *self, const char *namespace)
 	/* WARN: only one namespace supported */
 	unused(self);
 	unused(namespace);
-	return 0;
+	return -EALREADY;
 }
 
 static void flash_kvstore_close(struct kvstore *self)
@@ -241,19 +368,26 @@ struct kvstore *flash_kvstore_new(struct flash *flash, struct flash *scratch)
 	};
 
 	nvs.storage.flash = flash;
-	nvs.storage.flash_scratch = scratch;
 	nvs.storage.size = flash_size(flash);
+	nvs.storage.offset = 0;
 
 	/* meta takes about 6% of the given space */
-	size_t meta_size = nvs.storage.size >> 4;
-	size_t data_size = nvs.storage.size - meta_size;
-	uintptr_t meta_offset = 0;
-	uintptr_t data_offset = 0;
+	nvs.storage.meta.size = nvs.storage.size >> 4;
+	nvs.storage.data.size = nvs.storage.size - nvs.storage.meta.size;
+	nvs.storage.meta.offset = 0;
+	nvs.storage.data.offset = 0;
 
-	nvs.storage.meta.offset = meta_offset;
-	nvs.storage.meta.size = meta_size;
-	nvs.storage.data.offset = data_offset;
-	nvs.storage.data.size = data_size;
+	nvs.scratch.flash = scratch;
+	nvs.scratch.offset = 0;
+	if (scratch) {
+		nvs.scratch.size = flash_size(scratch);
+
+		nvs.scratch.meta.size = nvs.scratch.size >> 4;
+		nvs.scratch.data.size =
+			nvs.scratch.size - nvs.scratch.meta.size;
+		nvs.scratch.meta.offset = 0;
+		nvs.scratch.data.offset = 0;
+	}
 
 	return &nvs;
 }
--- tests/src/common/flash_kvstore_test.cpp
+++ tests/src/common/flash_kvstore_test.cpp
@@ -19,25 +19,59 @@ struct flash {
 	struct flash_api api;
 };
 
-static size_t fake_size(struct flash *self)
-{
+static uint8_t spy_flash[STORAGE_SIZE];
+static uint8_t spy_flash_scratch[STORAGE_SIZE];
+
+static size_t spy_size(struct flash *self) {
+	return sizeof(spy_flash);
+}
+static int spy_erase(struct flash *self, uintptr_t offset, size_t size) {
+	memset(&spy_flash[offset], 0xff, size);
+	return 0;
+}
+static int spy_write(struct flash *self,
+		uintptr_t offset, const void *data, size_t len) {
+	memcpy(&spy_flash[offset], data, len);
+	return 0;
+}
+static int spy_read(struct flash *self,
+		uintptr_t offset, void *buf, size_t len) {
+	memcpy(buf, &spy_flash[offset], len);
+	return (int)len;
+}
+
+static size_t spy_scratch_size(struct flash *self) {
+	return sizeof(spy_flash_scratch);
+}
+static int spy_scratch_erase(struct flash *self, uintptr_t offset, size_t size) {
+	memset(&spy_flash_scratch[offset], 0xff, size);
+	return 0;
+}
+static int spy_scratch_write(struct flash *self,
+		uintptr_t offset, const void *data, size_t len) {
+	memcpy(&spy_flash_scratch[offset], data, len);
+	return 0;
+}
+static int spy_scratch_read(struct flash *self,
+		uintptr_t offset, void *buf, size_t len) {
+	memcpy(buf, &spy_flash_scratch[offset], len);
+	return (int)len;
+}
+
+static size_t fake_size(struct flash *self) {
 	return (size_t)mock().actualCall(__func__)
 		.withParameter("self", self)
 		.returnUnsignedIntValueOrDefault(0);
 }
-
-static int fake_erase(struct flash *self, uintptr_t offset, size_t size)
-{
+static int fake_erase(struct flash *self, uintptr_t offset, size_t size) {
 	return mock().actualCall(__func__)
 		.withParameter("self", self)
 		.withParameter("offset", offset)
 		.withParameter("size", size)
 		.returnIntValueOrDefault(0);
 }
-
 static int fake_write(struct flash *self,
-		uintptr_t offset, const void *data, size_t len)
-{
+		uintptr_t offset, const void *data, size_t len) {
 	ByteArray byteArray = {
 		data, len,
 	};
@@ -47,10 +81,8 @@ static int fake_write(struct flash *self,
 		.withParameterOfType("ByteArray", "byteArray", &byteArray)
 		.returnIntValueOrDefault(0);
 }
-
 static int fake_read(struct flash *self,
-		uintptr_t offset, void *buf, size_t len)
-{
+		uintptr_t offset, void *buf, size_t len) {
 	return mock().actualCall(__func__)
 		.withParameter("self", self)
 		.withParameter("offset", offset)
@@ -58,9 +90,6 @@ static int fake_read(struct flash *self,
 		.withParameter("len", len)
 		.returnIntValueOrDefault(0);
 }
-
-static const uint32_t empty_line[] =
-		{ 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff };
 
 TEST_GROUP(FlashKVStore) {
 	ByteArrayComparator comparator;
@@ -76,6 +105,9 @@ TEST_GROUP(FlashKVStore) {
 		flash.api.read = fake_read;
 		flash.api.size = fake_size;
 
+		memset(spy_flash, 0xff, sizeof(spy_flash));
+		memset(spy_flash_scratch, 0xff, sizeof(spy_flash_scratch));
+
 		mock().expectOneCall("fake_size")
 			.ignoreOtherParameters()
 			.andReturnValue(STORAGE_SIZE);
@@ -88,6 +120,9 @@ TEST_GROUP(FlashKVStore) {
 	}
 
 	void prepare_meta_read_empty(int start, int n) {
+		static const uint32_t empty_line[] =
+			{ 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff };
+
 		for (int i = start; i < start + n; i++) {
 			mock().expectOneCall("fake_read")
 				.withParameter("self", &flash)
@@ -110,8 +145,9 @@ TEST_GROUP(FlashKVStore) {
 	}
 	void write_key_value(const char *key, const void *value, size_t value_size) {
 		prepare_meta_read_empty(0, META_SIZE / META_ENTRY_SIZE); // for alloc
+#if defined(FLASH_OVERWRITE)
 		prepare_meta_read_any(0, META_SIZE / META_ENTRY_SIZE); // for find
-
+#endif
 		mock().expectNCalls(2, "fake_write") // for write meta and value
 			.ignoreOtherParameters()
 			.andReturnValue(0);
@@ -120,14 +156,16 @@ TEST_GROUP(FlashKVStore) {
 	}
 };
 
-TEST(FlashKVStore, open_ShouldAlwaysReturnSuccess) {
-	LONGS_EQUAL(0, kvstore_open(kvstore, NULL));
-	LONGS_EQUAL(0, kvstore_open(kvstore, "namespace"));
+TEST(FlashKVStore, open_ShouldAlwaysReturnAlready) {
+	LONGS_EQUAL(-EALREADY, kvstore_open(kvstore, NULL));
+	LONGS_EQUAL(-EALREADY, kvstore_open(kvstore, "namespace"));
 }
 
 TEST(FlashKVStore, write_ShouldSucceed) {
 	prepare_meta_read_empty(0, META_SIZE / META_ENTRY_SIZE); // for alloc
+#if defined(FLASH_OVERWRITE)
 	prepare_meta_read_any(0, META_SIZE / META_ENTRY_SIZE); // for find
+#endif
 
 	const uint8_t expected_value[] = {
 		'm', 'y', 'v', 'a', 'l', 'u', 'e',
@@ -153,6 +191,7 @@ TEST(FlashKVStore, write_ShouldSucceed) {
 			expected_value, sizeof(expected_value)));
 }
 
+#if defined(FLASH_OVERWRITE)
 TEST(FlashKVStore, write_ShouldDeleteFirst_WhenTheSameEntryExist) {
 	const uint8_t new_value[] = {
 		'n', 'e', 'w', ' ', 'v', 'a', 'l', 'u', 'e',
@@ -212,12 +251,7 @@ TEST(FlashKVStore, write_ShouldDeleteFirst_WhenTheSameEntryExist) {
 	LONGS_EQUAL(0, kvstore_write(kvstore,
 			"mykey", new_value, sizeof(new_value)));
 }
-
-TEST(FlashKVStore, write_ShouldReturnNoSpace_WhenOutOfMemory) {
-}
-
-TEST(FlashKVStore, write_ShouldReclaim_WhenNoSpaceDueToFragmentation) {
-}
+#endif
 
 TEST(FlashKVStore, read_ShouldReturnNoEntry_WhenGivenKeyNotFound) {
 	uint8_t buf[64];
@@ -242,8 +276,9 @@ TEST(FlashKVStore, read_ShouldReturnLengthOfBytesRead) {
 		.withOutputParameterReturning("buf",
 				exist_meta, sizeof(exist_meta))
 		.andReturnValue(0);
+	prepare_meta_read_empty(1, META_SIZE / META_ENTRY_SIZE - 1);
 
-	mock().expectOneCall("fake_read") // for find
+	mock().expectOneCall("fake_read") // for value read
 		.withParameter("self", &flash)
 		.withParameter("offset", 64)
 		.withParameter("len", sizeof(data))
@@ -255,10 +290,15 @@ TEST(FlashKVStore, read_ShouldReturnLengthOfBytesRead) {
 }
 
 TEST(FlashKVStore, erase_ShouldReturnNoEntry_WhenNonExistKeyGiven) {
+#if defined(FLASH_OVERWRITE)
 	prepare_meta_read_any(0, META_SIZE / META_ENTRY_SIZE);
 	LONGS_EQUAL(-ENOENT, kvstore_clear(kvstore, "nonexistkey"));
+#else
+	LONGS_EQUAL(-ENOTSUP, kvstore_clear(kvstore, "nonexistkey"));
+#endif
 }
 
+#if defined(FLASH_OVERWRITE)
 TEST(FlashKVStore, erase_ShouldDeleteMetaEntry_WhenMatchingKeyGiven) {
 	const uint8_t data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	const char *key = "key string";
@@ -288,4 +328,149 @@ TEST(FlashKVStore, erase_ShouldDeleteMetaEntry_WhenMatchingKeyGiven) {
 		.andReturnValue(0);
 
 	LONGS_EQUAL(0, kvstore_clear(kvstore, key));
+}
+#endif
+
+TEST(FlashKVStore, read_ShouldReturnLatestEntry_WhenTheSameKeyInMultipleEntriesGiven) {
+	uint8_t buf[64];
+	const uint8_t first_data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	const uint8_t second_data[] = { 0xa, 0xb, 0xc };
+	const uint8_t first_meta[] = {
+		0xab, 0x13, 0x35, 0x24, 0x25, 0xff, 0x4a, 0x3a,
+		0x00, 0x00, 0x00, 0x00, sizeof(first_data), 0x00, 0x00, 0x00,
+	};
+	const uint8_t second_meta[] = {
+		0xab, 0x13, 0x35, 0x24, 0x25, 0xff, 0x4a, 0x3a,
+		META_ENTRY_SIZE, 0x00, 0x00, 0x00, sizeof(second_data), 0x00, 0x00, 0x00,
+	};
+	const char *key = "key string";
+
+	mock().expectOneCall("fake_read") // for find
+		.withParameter("self", &flash)
+		.withParameter("offset", 0)
+		.withParameter("len", sizeof(first_meta))
+		.withOutputParameterReturning("buf",
+				first_meta, sizeof(first_meta))
+		.andReturnValue((int)sizeof(first_meta));
+	mock().expectOneCall("fake_read")
+		.withParameter("self", &flash)
+		.withParameter("offset", META_ENTRY_SIZE)
+		.withParameter("len", sizeof(second_meta))
+		.withOutputParameterReturning("buf",
+				second_meta, sizeof(second_meta))
+		.andReturnValue((int)sizeof(second_meta));
+	prepare_meta_read_empty(2, META_SIZE / META_ENTRY_SIZE - 2);
+
+	mock().expectOneCall("fake_read") // for value read
+		.withParameter("self", &flash)
+		.withParameter("offset", 64 + META_ENTRY_SIZE)
+		.withParameter("len", sizeof(second_data))
+		.withOutputParameterReturning("buf",
+				second_data, sizeof(second_data))
+		.andReturnValue((int)sizeof(second_data));
+
+	LONGS_EQUAL(sizeof(second_data), kvstore_read(kvstore, key, buf, sizeof(buf)));
+	MEMCMP_EQUAL(second_data, buf, sizeof(second_data));
+}
+
+TEST(FlashKVStore, write_ShouldWriteInAlignment) {
+	const uint8_t first_meta[] = {
+		0xab, 0x13, 0x35, 0x24, 0x25, 0xff, 0x4a, 0x3a,
+		0x00, 0x00, 0x00, 0x00, 9, 0x00, 0x00, 0x00,
+	};
+
+	mock().expectOneCall("fake_read") // for find
+		.withParameter("self", &flash)
+		.withParameter("offset", 0)
+		.withParameter("len", sizeof(first_meta))
+		.withOutputParameterReturning("buf",
+				first_meta, sizeof(first_meta))
+		.andReturnValue((int)sizeof(first_meta));
+	prepare_meta_read_empty(1, META_SIZE / META_ENTRY_SIZE - 1);
+
+	const uint8_t expected_value[] = { 1, 2, 3 };
+	const uint8_t expected_meta[] = {
+		0x34, 0xfd, 0x64, 0x71, 0x94, 0x9d, 0xf7, 0x0f,
+		META_ENTRY_SIZE, 0x00, 0x00, 0x00, sizeof(expected_value), 0x00, 0x00, 0x00,
+	};
+	ByteArray meta = { expected_meta, sizeof(expected_meta), };
+	ByteArray value = { expected_value, sizeof(expected_value), };
+	mock().expectOneCall("fake_write") // for write meta
+		.withParameter("self", &flash)
+		.withParameter("offset", META_ENTRY_SIZE)
+		.withParameterOfType("ByteArray", "byteArray", &meta)
+		.andReturnValue(0);
+	mock().expectOneCall("fake_write") // for write value
+		.withParameter("self", &flash)
+		.withParameter("offset", 64 + META_ENTRY_SIZE)
+		.withParameterOfType("ByteArray", "byteArray", &value)
+		.andReturnValue(0);
+
+	LONGS_EQUAL(0, kvstore_write(kvstore, "mykey",
+			expected_value, sizeof(expected_value)));
+}
+
+TEST(FlashKVStore, write_ShouldReturnNoSpace_WhenNoSpaceLeft) {
+	struct flash f = {
+		.api = {
+			.erase = spy_erase,
+			.write = spy_write,
+			.read = spy_read,
+			.size = spy_size,
+		},
+	};
+	struct flash s = {
+		.api = {
+			.erase = spy_scratch_erase,
+			.write = spy_scratch_write,
+			.read = spy_scratch_read,
+			.size = spy_scratch_size,
+		},
+	};
+	kvstore = flash_kvstore_new(&f, &s);
+	const char *keys[] = {
+		"key1",
+		"key2",
+		"key3",
+		"key4",
+		"key5",
+	};
+	const uint8_t data[] = { 0xde, 0xad, 0xc0, 0xde };
+	for (int i = 0; i < 4; i++) {
+		LONGS_EQUAL(0, kvstore_write(kvstore, keys[i], data, sizeof(data)));
+	}
+	LONGS_EQUAL(-ENOSPC, kvstore_write(kvstore, keys[4], data, sizeof(data)));
+}
+
+TEST(FlashKVStore, write_ShouldReclaim_WhenNoSpaceDueToFragmentation) {
+	struct flash f = {
+		.api = {
+			.erase = spy_erase,
+			.write = spy_write,
+			.read = spy_read,
+			.size = spy_size,
+		},
+	};
+	struct flash s = {
+		.api = {
+			.erase = spy_scratch_erase,
+			.write = spy_scratch_write,
+			.read = spy_scratch_read,
+			.size = spy_scratch_size,
+		},
+	};
+	kvstore = flash_kvstore_new(&f, &s);
+	const char *keys[] = {
+		"key1",
+		"key1",
+		"key3",
+		"key4",
+		"key4",
+		"key5",
+	};
+	const uint8_t data[][1] = { 1, 2, 3, 4, 5, 6 };
+	for (int i = 0; i < 5; i++) {
+		LONGS_EQUAL(0, kvstore_write(kvstore, keys[i], data[i], sizeof(data[i])));
+	}
+	LONGS_EQUAL(0, kvstore_write(kvstore, keys[5], data[5], sizeof(data[5])));
 }
