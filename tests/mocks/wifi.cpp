641ef33b33c515aa669af903953e0b5cd7ebebb0
Kyunghwan Kwon <k@mononn.com>
2024-02-14 10:22:25 +0900

    update wifi mock

--- tests/mocks/wifi.cpp
+++ tests/mocks/wifi.cpp
@@ -1,21 +1,16 @@
 #include "CppUTestExt/MockSupport.h"
 #include "libmcu/wifi.h"
 
+struct wifi {
+	struct wifi_api api;
+};
+
 static struct wifi *iface;
 static wifi_event_callback_t callback;
 static void *callback_ctx;
 
-int wifi_enable(struct wifi *self) {
-	iface = self;
-	return mock().actualCall(__func__).returnIntValue();
-}
-
-int wifi_disable(struct wifi *self) {
-	return mock().actualCall(__func__).returnIntValue();
-}
-
-int wifi_connect(struct wifi *self, const struct wifi_conn_param *param) {
-	int rc = mock().actualCall(__func__).returnIntValue();
+static int do_connect(struct wifi *self, const struct wifi_conn_param *param) {
+	int rc = mock().actualCall("wifi_connect").returnIntValue();
 	enum wifi_event evt = rc? WIFI_EVT_DISCONNECTED : WIFI_EVT_CONNECTED;
 	if (callback) {
 		(*callback)(iface, evt, 0, callback_ctx);
@@ -23,16 +18,46 @@ int wifi_connect(struct wifi *self, const struct wifi_conn_param *param) {
 	return rc;
 }
 
-int wifi_disconnect(struct wifi *self) {
-	return mock().actualCall(__func__).returnIntValue();
+static int do_disconnect(struct wifi *self) {
+	return mock().actualCall("wifi_disconnect").returnIntValue();
 }
 
-int wifi_scan(struct wifi *self) {
-	return mock().actualCall(__func__).returnIntValue();
+static int do_scan(struct wifi *self) {
+	return mock().actualCall("wifi_scan").returnIntValue();
 }
 
-int wifi_register_event_callback(struct wifi *self, enum wifi_event event_type, const wifi_event_callback_t cb, void *user_ctx) {
+static int do_enable(struct wifi *self) {
+	iface = self;
+	return mock().actualCall("wifi_enable").returnIntValue();
+}
+
+static int do_disable(struct wifi *self) {
+	return mock().actualCall("wifi_disable").returnIntValue();
+}
+
+static int do_register_event_callback(struct wifi *self,
+		enum wifi_event event_type,
+		const wifi_event_callback_t cb, void *user_ctx) {
 	callback = cb;
 	callback_ctx = user_ctx;
-	return mock().actualCall(__func__).returnIntValue();
+	return mock().actualCall("wifi_register_event_callback").returnIntValue();
+}
+
+struct wifi *wifi_create(int id) {
+	static struct wifi iface = {
+		.api = {
+			.connect = do_connect,
+			.disconnect = do_disconnect,
+			.scan = do_scan,
+			.enable = do_enable,
+			.disable = do_disable,
+			.register_event_callback = do_register_event_callback,
+		},
+	};
+	return &iface;
+}
+
+int wifi_delete(struct wifi *self) {
+	(void)self;
+	return 0;
 }
