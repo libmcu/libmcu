801d1cfb37d940975113a88ce79c47435d7e1f8f
Kyunghwan Kwon <k@mononn.com>
2024-06-15 22:45:33 +0900

    port: freertos: add calloc wrapper (#175)

--- ports/freertos/malloc.c
+++ ports/freertos/malloc.c
@@ -5,6 +5,8 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -15,6 +17,21 @@ void *malloc(size_t size)
 	}
 
 	return pvPortMalloc(size);
+}
+
+void *calloc(size_t num, sizeo_t size)
+{
+	if (num == 0 || size == 0) {
+		return NULL;
+	}
+
+	void *p = pvPortMalloc(num *size);
+
+	if (p) {
+		memset(p, 0, num * size);
+	}
+
+	return p;
 }
 
 void free(void *ptr)
