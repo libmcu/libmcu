f01edc8753afa3636b371e781927e38e8778c7ca
Kyunghwan Kwon <k@mononn.com>
2024-09-16 07:27:18 +0900

    interface: spi: add pin parameter to spi_create function (#188)

--- interfaces/spi/include/libmcu/spi.h
+++ interfaces/spi/include/libmcu/spi.h
@@ -14,10 +14,17 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
+typedef enum {
+	SPI_MODE_0, /* CPOL=0, CPHA=0 */
+	SPI_MODE_1, /* CPOL=0, CPHA=1 */
+	SPI_MODE_2, /* CPOL=1, CPHA=0 */
+	SPI_MODE_3, /* CPOL=1, CPHA=1 */
+} spi_mode_t;
+
 struct spi;
 
 struct spi_api {
-	int (*enable)(struct spi *self);
+	int (*enable)(struct spi *self, spi_mode_t mode, uint32_t freq_hz);
 	int (*disable)(struct spi *self);
 	int (*write)(struct spi *self, const void *data, size_t data_len);
 	int (*read)(struct spi *self, void *buf, size_t bufsize);
@@ -25,8 +32,15 @@ struct spi_api {
 			size_t txdata_len, void *rxbuf, size_t rxbuf_len);
 };
 
-static inline int spi_enable(struct spi *self) {
-	return ((struct spi_api *)self)->enable(self);
+struct spi_pin {
+	int miso;
+	int mosi;
+	int sclk;
+};
+
+static inline int spi_enable(struct spi *self,
+		spi_mode_t mode, uint32_t freq_hz) {
+	return ((struct spi_api *)self)->enable(self, mode, freq_hz);
 }
 
 static inline int spi_disable(struct spi *self) {
@@ -48,7 +62,7 @@ static inline int spi_writeread(struct spi *self, const void *txdata,
 			txdata, txdata_len, rxbuf, rxbuf_len);
 }
 
-struct spi *spi_create(uint8_t channel);
+struct spi *spi_create(uint8_t channel, const struct spi_pin *pin);
 void spi_delete(struct spi *self);
 
 #if defined(__cplusplus)
