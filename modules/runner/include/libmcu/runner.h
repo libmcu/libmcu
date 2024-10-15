d53a0db3f6ef45b1cc24d4b6fc42aedcec288f1f
Kyunghwan Kwon <k@mononn.com>
2024-10-15 15:20:07 +0900

    runner: add context to runner module (#196)

--- modules/runner/include/libmcu/runner.h
+++ modules/runner/include/libmcu/runner.h
@@ -24,7 +24,7 @@ typedef uint8_t runner_t;
 typedef bool (*runner_change_cb_t)(const runner_t type, void *ctx);
 
 struct runner_api {
-	int (*prepare)(void);
+	int (*prepare)(void *ctx);
 	void (*terminate)(void);
 	int (*step)(uint32_t *until_next_period_ms);
 	void (*run)(void);
@@ -42,27 +42,28 @@ struct runner {
 /**
  * @brief This function initializes the runner module with an array of runners.
  *
- * @param runners[in] A pointer to the first element of an array of `struct
+ * @param[in] runners A pointer to the first element of an array of `struct
  *                    runner` that represents the runners to be managed by the
  *                    module.
- * @param nr_runners[in] The number of runners in the array.
+ * @param[in] nr_runners The number of runners in the array.
+ * @param[in] ctx The context of runner
  *
  * @return int Returns 0 if the initialization was successful, and a non-zero
  *             error code otherwise.
  */
-int runner_init(const struct runner *runners, size_t nr_runners);
+int runner_init(const struct runner *runners, size_t nr_runners, void *ctx);
 
 /**
  * @breif This function registers callback functions that are called before and
  *        after the runner changes its state.
  *
- * @param pre_cb[in] A pointer to the function to be called before the runner
+ * @param[in] pre_cb A pointer to the function to be called before the runner
  *                   changes its state.
- * @param pre_ctx[in] A void pointer that will be passed as the argument to the
+ * @param[in] pre_ctx A void pointer that will be passed as the argument to the
  *                    pre_cb function.
- * @param post_cb[in] A pointer to the function to be called after the runner
+ * @param[in] post_cb A pointer to the function to be called after the runner
  *                    changes its state.
- * @param post_ctx[in] A void pointer that will be passed as the argument to the
+ * @param[in] post_ctx A void pointer that will be passed as the argument to the
  *                     post_cb function.
  *
  * @return int Returns 0 if the callbacks were successfully registered, and a
