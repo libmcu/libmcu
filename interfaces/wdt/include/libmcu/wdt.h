b6c53f5a13eeff427d67318d13bf1816f0cd232b
Kyunghwan Kwon <k@mononn.com>
2024-12-04 18:42:05 +0900

    implement for esp32

--- interfaces/wdt/include/libmcu/wdt.h
+++ interfaces/wdt/include/libmcu/wdt.h
@@ -11,6 +11,8 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 struct wdt;
 
 /**
@@ -22,14 +24,16 @@ struct wdt;
 typedef void (*wdt_timeout_cb_t)(struct wdt *wdt, void *ctx);
 
 /**
- * @brief Initialize the watchdog timer.
+ * @brief Initialize the watchdog timer module.
  *
- * @param[in] cb Callback function to be called on watchdog timeout.
- * @param[in] cb_ctx User-defined context to be passed to the callback function.
+ * This function initializes the watchdog timer module. It should be called
+ * before any other watchdog timer functions are used. This function sets up
+ * the necessary hardware or software resources required for the watchdog
+ * timer to operate.
  *
  * @return 0 on success, negative error code on failure.
  */
-int wdt_init(wdt_timeout_cb_t cb, void *cb_ctx);
+int wdt_init(void);
 
 /**
  * @brief Deinitialize the watchdog timer.
@@ -41,12 +45,40 @@ int wdt_init(wdt_timeout_cb_t cb, void *cb_ctx);
 void wdt_deinit(void);
 
 /**
+ * @brief Register a callback function for watchdog timeout.
+ *
+ * This function registers a callback function that will be called when the
+ * watchdog timer times out. The callback function can be used to perform
+ * necessary actions before the system resets.
+ *
+ * @param[in] cb Callback function to be called on watchdog timeout.
+ * @param[in] cb_ctx User-defined context to be passed to the callback function.
+ *
+ * @note The callback registered here will be called when any of the watchdog
+ *       timers times out.
+ * @note Only one callback can be registered at a time.
+ *
+ * @return 0 on success, negative error code on failure.
+ */
+int wdt_register_timeout_cb(wdt_timeout_cb_t cb, void *cb_ctx);
+
+/**
  * @brief Create a new watchdog timer instance.
+ *
+ * @param[in] period_ms The timeout period in milliseconds for the watchdog
+ *                      timer.
+ * @param[in] cb Callback function to be called on watchdog timeout.
+ * @param[in] cb_ctx User-defined context to be passed to the callback function.
+ *
+ * @note The callback registered here will be called when this watchdog timer
+ *       times out and then the callback registered in wdt_init() will be
+ *       called.
  *
  * @return Pointer to the newly created watchdog timer instance,
  *                 or NULL on failure.
  */
-struct wdt *wdt_new(void);
+struct wdt *wdt_new(const uint32_t period_ms,
+		wdt_timeout_cb_t cb, void *cb_ctx);
 
 /**
  * @brief Delete a watchdog timer instance.
--- ports/esp-idf/wdt.c
+++ ports/esp-idf/wdt.c
@@ -5,53 +5,135 @@
  */
 
 #include "libmcu/wdt.h"
+
 #include <stdlib.h>
+#include <pthread.h>
+
 #include "esp_task_wdt.h"
 #include "libmcu/board.h"
+#include "libmcu/list.h"
+#include "libmcu/timext.h"
+
+#define STACK_SIZE_BYTES	1024
+
+#if !defined(MIN)
+#define MIN(a, b)		((a) > (b)? (b) : (a))
+#endif
 
 struct wdt {
 	void *task_handle;
+
+	uint32_t period_ms; /* timeout period in milliseconds */
+	uint32_t last_feed_ms; /* last feed time in milliseconds */
+
+	wdt_timeout_cb_t cb;
+	void *cb_ctx;
+
+	struct list link;
 };
 
 struct wdt_manager {
+	pthread_t thread;
+
+	struct list list;
+
 	wdt_timeout_cb_t cb;
 	void *cb_ctx;
+
+	uint32_t min_period_ms;
 };
 
-static struct wdt_manager mgr;
+static struct wdt_manager m;
+
+static bool is_timedout(struct wdt *wdt, const uint32_t now)
+{
+	return (now - wdt->last_feed_ms) >= wdt->period_ms;
+}
+
+static struct wdt *any_timeouts(struct wdt_manager *mgr, const uint32_t now)
+{
+	struct list *p;
+
+	list_for_each(p, &mgr->list) {
+		struct wdt *wdt = list_entry(p, struct wdt, link);
+		if (is_timedout(wdt, now)) {
+			return wdt;
+		}
+	}
+
+	return NULL;
+}
+
+static void *wdt_task(void *e)
+{
+	struct wdt_manager *mgr = (struct wdt_manager *)e;
+	ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
+
+	while (1) {
+		struct wdt *wdt = any_timeouts(mgr,
+				board_get_time_since_boot_ms());
+
+		if (!wdt) {
+			esp_task_wdt_reset();
+			sleep_ms(mgr->min_period_ms);
+			continue;
+		}
+
+		if (wdt->cb) {
+			(*wdt->cb)(wdt, wdt->cb_ctx);
+		}
+		if (mgr->cb) {
+			(*mgr->cb)(wdt, mgr->cb_ctx);
+		}
+
+		sleep_ms(mgr->min_period_ms);
+	}
+
+	esp_task_wdt_delete(NULL);
+	return 0;
+}
 
 int wdt_feed(struct wdt *self)
 {
-	(void)self;
-	int err = esp_task_wdt_reset();
-	return err == ESP_OK ? 0 : -err;
+	self->last_feed_ms = board_get_time_since_boot_ms();
+	return 0;
 }
 
-struct wdt *wdt_new(void)
+struct wdt *wdt_new(const uint32_t period_ms, wdt_timeout_cb_t cb, void *cb_ctx)
 {
-	struct wdt *self = (struct wdt *)malloc(sizeof(*self));
+	struct wdt *wdt = (struct wdt *)malloc(sizeof(*wdt));
 
-	if (self) {
-		self->task_handle = board_get_current_thread();
+	if (wdt) {
+		wdt->period_ms = period_ms;
+		wdt->cb = cb;
+		wdt->cb_ctx = cb_ctx;
+		wdt->task_handle = board_get_current_thread();
+		list_add(&wdt->link, &m.list);
+
+		m.min_period_ms = MIN(m.min_period_ms, period_ms);
 	}
 	
-	int err = esp_task_wdt_add(NULL);
-
-	return err == ESP_OK? self : NULL;
+	return wdt;
 }
 
 void wdt_delete(struct wdt *self)
 {
-	esp_task_wdt_delete(NULL);
+	list_del(&self->link, &m.list);
 	free(self);
 }
 
-int wdt_init(wdt_timeout_cb_t cb, void *cb_ctx)
+int wdt_register_timeout_cb(wdt_timeout_cb_t cb, void *cb_ctx)
 {
-	int err = 0;
+	m.cb = cb;
+	m.cb_ctx = cb_ctx;
 
-	mgr.cb = cb;
-	mgr.cb_ctx = cb_ctx;
+	return 0;
+}
+
+int wdt_init(void)
+{
+	m.min_period_ms = CONFIG_TASK_WDT_TIMEOUT_S * 1000;
+	list_init(&m.list);
 
 #if !CONFIG_ESP_TASK_WDT_INIT
 	esp_task_wdt_config_t twdt_config = {
@@ -59,15 +141,24 @@ int wdt_init(wdt_timeout_cb_t cb, void *cb_ctx)
 		.idle_core_mask = (1 << CONFIG_FREERTOS_NUMBER_OF_CORES) - 1,
 		.trigger_panic = false,
 	};
-	err = esp_task_wdt_init(&twdt_config);
-	err = err == ESP_OK? 0 : -err;
+	int err = esp_task_wdt_init(&twdt_config);
+
+	if (err != ESP_OK) {
+		return -err;
+	}
 #endif
 
-	return err;
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setstacksize(&attr, STACK_SIZE_BYTES);
+
+	return pthread_create(&m.thread, &attr, wdt_task, &m);
 }
 
 void wdt_deinit(void)
 {
+	pthread_exit(&m.thread);
 #if !CONFIG_ESP_TASK_WDT_INIT
 	ESP_ERROR_CHECK(esp_task_wdt_deinit());
 #endif
