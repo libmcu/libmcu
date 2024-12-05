9f5a851643901a6af45e15207318ff963998aab1
Kyunghwan Kwon <k@mononn.com>
2024-12-05 10:56:38 +0900

    Add cleanup module with priority-based execution (#223)

---
+++ modules/cleanup/include/libmcu/cleanup.h
@@ -0,0 +1,70 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef LIBMCU_CLEANUP_H
+#define LIBMCU_CLEANUP_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/**
+ * @brief Enumeration of possible cleanup errors.
+ */
+typedef enum {
+	CLEANUP_ERROR_NONE,           /**< No error occurred. */
+	CLEANUP_ERROR_ALLOC_FAILED,   /**< Allocation failed. */
+} cleanup_error_t;
+
+/**
+ * @brief Type definition for cleanup function pointer.
+ *
+ * @param[in] ctx Context pointer to be passed to the cleanup function.
+ */
+typedef void (*cleanup_func_t)(void *ctx);
+
+/**
+ * @brief Initialize the cleanup module.
+ *
+ * @return CLEANUP_ERROR_NONE on success, or an appropriate error code on
+ *         failure.
+ */
+cleanup_error_t cleanup_init(void);
+
+/**
+ * @brief Deinitialize the cleanup module.
+ */
+void cleanup_deinit(void);
+
+/**
+ * @brief Register a cleanup function to be called during cleanup.
+ *
+ * @param[in] priority The priority of the cleanup function. Higher priority
+ *                     functions are called first.
+ * @param[in] func The cleanup function to register.
+ * @param[in] func_ctx The context to be passed to the cleanup function.
+ *
+ * @return CLEANUP_ERROR_NONE on success, or an appropriate error code on
+ *         failure.
+ */
+cleanup_error_t cleanup_register(int priority,
+		cleanup_func_t func, void *func_ctx);
+
+/**
+ * @brief Execute all registered cleanup functions in order of their priority.
+ *
+ * This function will call all registered cleanup functions in order of their
+ * priority, from highest to lowest. When the same priority is given, the
+ * cleanup functions are called first come, first serve style (FIFO) in the
+ * order they were registered.
+ */
+void cleanup_execute(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* LIBMCU_CLEANUP_H */
---
+++ tests/src/cleanup/cleanup_test.cpp
@@ -0,0 +1,68 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/TestHarness_c.h"
+#include "CppUTestExt/MockSupport.h"
+
+#include "libmcu/cleanup.h"
+
+TEST_GROUP(Cleanup) {
+	void setup(void) {
+		cleanup_init();
+	}
+	void teardown(void) {
+		mock().checkExpectations();
+		mock().clear();
+
+		cleanup_deinit();
+	}
+};
+
+static void test_func1(void *ctx) {
+	mock().actualCall(__func__);
+}
+
+static void test_func2(void *ctx) {
+	mock().actualCall(__func__);
+}
+
+TEST(Cleanup, ShouldReturnNoError_WhenValidFunctionIsRegistered) {
+	LONGS_EQUAL(CLEANUP_ERROR_NONE, cleanup_register(1, test_func1, NULL));
+	LONGS_EQUAL(CLEANUP_ERROR_NONE, cleanup_register(2, test_func2, NULL));
+}
+
+TEST(Cleanup, ShouldRegisteredFunctionsBeCalled_WhenCleanupIsExecuted) {
+	mock().expectOneCall("test_func1");
+	mock().expectOneCall("test_func2");
+
+	cleanup_register(1, test_func1, NULL);
+	cleanup_register(2, test_func2, NULL);
+
+	cleanup_execute();
+}
+
+TEST(Cleanup, ShouldRegisteredFunctionsBeCalledInOrderOfPriority_WhenCleanupIsExecuted) {
+	mock().strictOrder();
+	mock().expectOneCall("test_func2");
+	mock().expectOneCall("test_func1");
+
+	cleanup_register(1, test_func1, NULL);
+	cleanup_register(2, test_func2, NULL);
+
+	cleanup_execute();
+}
+
+TEST(Cleanup, ShouldRegisteredFunctionsBeCalledInFirstComeFirstServeOrder_WhenTheSamePriorityGiven) {
+	mock().strictOrder();
+	mock().expectOneCall("test_func1");
+	mock().expectOneCall("test_func2");
+
+	cleanup_register(1, test_func1, NULL);
+	cleanup_register(1, test_func2, NULL);
+
+	cleanup_execute();
+}
