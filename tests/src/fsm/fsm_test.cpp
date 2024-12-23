2849aa6b29167154cfd98bd6c286ef569e1a2b73
Kyunghwan Kwon <k@mononn.com>
2024-12-23 17:09:13 +0900

    add test cases

--- tests/src/fsm/fsm_test.cpp
+++ tests/src/fsm/fsm_test.cpp
@@ -11,75 +11,58 @@
 #include "libmcu/fsm.h"
 
 enum {
-	READY,
-	OCCUPIED,
-	CHARGING,
-	UNAVAILABLE,
+	A,
+	B,
+	C,
 };
 
-static bool is_plugged_in(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
+static bool is_state_a(fsm_state_t state, fsm_state_t next_state, void *ctx) {
 	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
 }
 
-static bool is_plugged_out(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
+static bool is_state_b(fsm_state_t state, fsm_state_t next_state, void *ctx) {
 	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
 }
 
-static bool has_rfid_tagged(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
+static bool is_state_c(fsm_state_t state, fsm_state_t next_state, void *ctx) {
 	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
 }
 
-static bool has_remotely_started(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool has_remotely_stopped(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool is_timed_out(fsm_state_t state, fsm_state_t next_state, void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool is_suspended_by_ev(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool is_resumed_from_suspended(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool has_hardware_error(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static bool is_hardware_recovered(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
-	return mock().actualCall(__func__).returnBoolValueOrDefault(false);
-}
-
-static void turn_relay_on(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
+static void do_state_a(fsm_state_t state, fsm_state_t next_state, void *ctx) {
 	mock().actualCall(__func__);
 }
 
-static void turn_relay_off(fsm_state_t state, fsm_state_t next_state,
-		void *ctx) {
+static void do_state_b(fsm_state_t state, fsm_state_t next_state, void *ctx) {
 	mock().actualCall(__func__);
 }
+
+static void do_state_c(fsm_state_t state, fsm_state_t next_state, void *ctx) {
+	mock().actualCall(__func__);
+}
+
+static void on_state_change(struct fsm *fsm, fsm_state_t new_state,
+		fsm_state_t prev_state, void *ctx) {
+	mock().actualCall(__func__).withParameter("new_state", new_state)
+		.withParameter("prev_state", prev_state);
+}
+
+static const struct fsm_item transitions[] = {
+	FSM_ITEM(A, NULL,       do_state_a, A),
+	FSM_ITEM(A, is_state_b, do_state_b, B),
+	FSM_ITEM(A, is_state_c, do_state_c, C),
+	FSM_ITEM(B, is_state_a, do_state_a, A),
+	FSM_ITEM(B, is_state_c, do_state_c, C),
+	FSM_ITEM(C, is_state_a, do_state_a, A),
+	FSM_ITEM(C, is_state_b, do_state_b, B),
+	FSM_ITEM(C, is_state_c, NULL,       C),
+};
 
 TEST_GROUP(FSM) {
 	struct fsm fsm;
 
 	void setup(void) {
+		fsm_init(&fsm, transitions,
+				sizeof(transitions) / sizeof(*transitions), 0);
 	}
 	void teardown(void) {
 		mock().checkExpectations();
@@ -87,41 +70,71 @@ TEST_GROUP(FSM) {
 	}
 };
 
-TEST(FSM, test) {
-	struct fsm_item items[] = {
-		FSM_ITEM(READY, is_plugged_in, NULL, OCCUPIED),
-		FSM_ITEM(READY, has_rfid_tagged, NULL, OCCUPIED),
-		FSM_ITEM(READY, has_remotely_started, NULL, OCCUPIED),
-		FSM_ITEM(OCCUPIED, is_plugged_in, turn_relay_on, CHARGING),
-		FSM_ITEM(OCCUPIED, has_rfid_tagged, turn_relay_on, CHARGING),
-		FSM_ITEM(OCCUPIED, has_remotely_started, turn_relay_on, CHARGING),
-		FSM_ITEM(OCCUPIED, is_timed_out, NULL, READY),
-		FSM_ITEM(OCCUPIED, is_plugged_out, NULL, READY),
-		FSM_ITEM(CHARGING, is_plugged_out, turn_relay_off, READY),
-		FSM_ITEM(CHARGING, has_rfid_tagged, turn_relay_off, OCCUPIED),
-		FSM_ITEM(CHARGING, has_remotely_stopped, turn_relay_off, OCCUPIED),
-		FSM_ITEM(CHARGING, is_suspended_by_ev, turn_relay_off, CHARGING),
-		FSM_ITEM(CHARGING, is_resumed_from_suspended, turn_relay_on, CHARGING),
-		FSM_ITEM(READY, has_hardware_error, NULL, UNAVAILABLE),
-		FSM_ITEM(OCCUPIED, has_hardware_error, NULL, UNAVAILABLE),
-		FSM_ITEM(CHARGING, has_hardware_error, NULL, UNAVAILABLE),
-		FSM_ITEM(UNAVAILABLE, is_hardware_recovered, NULL, READY),
-	};
+TEST(FSM, ShouldBeStateA_WhenInitialized) {
+	LONGS_EQUAL(A, fsm_state(&fsm));
+}
 
-	fsm_init(&fsm, items, sizeof(items) / sizeof(*items), 0);
+TEST(FSM, ShouldCheckFirstRegisteredEventFirst) {
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().ignoreOtherCalls();
+	fsm_step(&fsm);
+}
 
-	mock().expectOneCall("is_plugged_in").andReturnValue(true);
+TEST(FSM, ShouldCheckSecondRegisteredEvent_WhenFirstEventReturnFalse) {
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(false);
+	mock().ignoreOtherCalls();
 	fsm_step(&fsm);
-	mock().expectOneCall("is_plugged_in").andReturnValue(true);
-	mock().expectOneCall("turn_relay_on");
+}
+
+TEST(FSM, ShouldCheckAllEventsInThestate_WhenNoEventReturnTrue) {
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(false);
 	fsm_step(&fsm);
-	mock().expectOneCall("is_plugged_out").andReturnValue(false);
-	mock().expectOneCall("has_rfid_tagged").andReturnValue(false);
-	mock().expectOneCall("has_remotely_stopped").andReturnValue(false);
-	mock().expectOneCall("is_suspended_by_ev").andReturnValue(true);
-	mock().expectOneCall("turn_relay_off");
+}
+
+TEST(FSM, ShouldCallAction_WhenEventReturnTrue) {
+	mock().expectOneCall("is_state_b").andReturnValue(true);
+	mock().expectOneCall("do_state_b");
 	fsm_step(&fsm);
-	mock().expectOneCall("is_plugged_out").andReturnValue(true);
-	mock().expectOneCall("turn_relay_off");
+}
+
+TEST(FSM, ShouldChangeState_WhenEventReturnTrue) {
+	mock().expectOneCall("is_state_b").andReturnValue(true);
+	mock().expectOneCall("do_state_b");
+	LONGS_EQUAL(B, fsm_step(&fsm));
+	LONGS_EQUAL(B, fsm_state(&fsm));
+}
+
+TEST(FSM, ShouldIgnoreNullAction_WhenEventReturnTrue) {
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(true);
+	mock().expectOneCall("do_state_c");
+	fsm_step(&fsm);
+	mock().expectOneCall("is_state_a").andReturnValue(false);
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(true);
+	fsm_step(&fsm);
+}
+
+TEST(FSM, ShouldCallCallback_WhenStateChange) {
+	fsm_set_state_change_cb(&fsm, on_state_change, 0);
+	mock().expectOneCall("on_state_change").withParameter("new_state", B)
+		.withParameter("prev_state", A);
+	mock().expectOneCall("is_state_b").andReturnValue(true);
+	mock().expectOneCall("do_state_b");
+	fsm_step(&fsm);
+}
+
+TEST(FSM, ShouldNotCallCallback_WhenStateIsNotChanged) {
+	fsm_set_state_change_cb(&fsm, on_state_change, 0);
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(false);
+	fsm_step(&fsm);
+}
+
+TEST(FSM, ShouldIgnoreNullEvent) {
+	mock().expectOneCall("is_state_b").andReturnValue(false);
+	mock().expectOneCall("is_state_c").andReturnValue(false);
 	fsm_step(&fsm);
 }
