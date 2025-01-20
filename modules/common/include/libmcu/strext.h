0f9e8407c6b6844e782c8a7fd5133a0174efa4b0
Kyunghwan Kwon <k@mononn.com>
2025-01-20 17:59:17 +0900

    common/strext: add strupper and strlower functions

--- modules/common/include/libmcu/strext.h
+++ modules/common/include/libmcu/strext.h
@@ -21,6 +21,28 @@ extern "C" {
  */
 char *strtrim(char *s, const char c);
 
+/**
+ * @brief Converts all characters in the string to uppercase.
+ *
+ * This function iterates through each character in the input string
+ * and converts it to its uppercase equivalent.
+ *
+ * @param[in,out] s The input string to be converted. The string is modified
+ *                in place.
+ */
+void strupper(char *s);
+
+/**
+ * @brief Converts all characters in the string to lowercase.
+ *
+ * This function iterates through each character in the input string
+ * and converts it to its lowercase equivalent.
+ *
+ * @param[in,out] s The input string to be converted. The string is modified
+ *                in place.
+ */
+void strlower(char *s);
+
 #if defined(__cplusplus)
 }
 #endif
