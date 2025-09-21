957421f8629c0474938c8589b572d2f60cec52d7
Kyunghwan Kwon <k@pazzk.net>
2025-09-21 12:31:35 +0900

    test(timext): add mocks for ISO 8601 conversion

--- tests/mocks/timext.cpp
+++ tests/mocks/timext.cpp
@@ -12,3 +12,19 @@ void timeout_set(uint32_t *goal, uint32_t msec) {
 void sleep_ms(uint32_t msec) {
 	mock().actualCall(__func__).withParameter("msec", msec);
 }
+
+int iso8601_convert_to_string(time_t t, char *buf, size_t bufsize) {
+	return mock().actualCall(__func__)
+		.withParameter("t", t)
+		.withParameter("buf", buf)
+		.withParameter("bufsize", bufsize)
+		.returnIntValueOrDefault(0); // Success
+}
+
+#if defined(_GNU_SOURCE)
+time_t iso8601_convert_to_time(const char *tstr) {
+	return (time_t)mock().actualCall(__func__)
+		.withParameter("tstr", tstr)
+		.returnIntValueOrDefault(1640995200); // Default: 2022-01-01T00:00:00Z
+}
+#endif
