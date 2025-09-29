de83882a18933b4f836b9242715de59db72bd7af
Kyunghwan Kwon <k@pazzk.net>
2025-09-29 12:13:47 +0900

    chore(spi): fix spi mock implementation

--- tests/mocks/spi.cpp
+++ tests/mocks/spi.cpp
@@ -2,65 +2,65 @@
 #include "libmcu/spi.h"
 #include "bytearray.h"
 
-struct spi {
-	struct spi_api api;
-};
+struct lm_spi *lm_spi_create(uint8_t channel, const struct lm_spi_pin *pin) {
+	return (struct lm_spi *)mock().actualCall("lm_spi_create")
+		.withIntParameter("channel", channel)
+		.returnPointerValue();
+}
 
-static int enable_spi(struct spi *self) {
-	return mock().actualCall("spi_enable").withParameter("self", self)
+void lm_spi_destroy(struct lm_spi *self) {
+	mock().actualCall("lm_spi_destroy")
+		.withPointerParameter("self", self);
+}
+
+struct lm_spi_device *lm_spi_create_device(struct lm_spi *self,
+		lm_spi_mode_t mode, uint32_t freq_hz, int pin_cs) {
+	return (struct lm_spi_device *)mock().actualCall("lm_spi_create_device")
+		.withPointerParameter("self", self)
+		.withIntParameter("mode", mode)
+		.withUnsignedIntParameter("freq_hz", freq_hz)
+		.withIntParameter("pin_cs", pin_cs)
+		.returnPointerValue();
+}
+
+int lm_spi_delete_device(struct lm_spi_device *dev) {
+	return mock().actualCall("lm_spi_delete_device")
+		.withPointerParameter("dev", dev)
 		.returnIntValue();
 }
 
-static int disable_spi(struct spi *self) {
-	return mock().actualCall("spi_disable").withParameter("self", self)
+int lm_spi_enable(struct lm_spi_device *dev) {
+	return mock().actualCall("lm_spi_enable")
+		.withPointerParameter("dev", dev)
 		.returnIntValue();
 }
 
-static int write_spi(struct spi *self, const void *data, size_t data_len) {
-	return mock().actualCall("spi_write")
-		.withParameter("self", self)
-		.withParameter("data", data)
-		.withParameter("data_len", data_len)
+int lm_spi_disable(struct lm_spi_device *dev) {
+	return mock().actualCall("lm_spi_disable")
+		.withPointerParameter("dev", dev)
 		.returnIntValue();
 }
 
-static int read_spi(struct spi *self, void *buf, size_t bufsize) {
-	return mock().actualCall("spi_read")
-		.withParameter("self", self)
-		.withParameter("buf", buf)
-		.withParameter("bufsize", bufsize)
+int lm_spi_write(struct lm_spi_device *dev, const void *data, size_t data_len) {
+	return mock().actualCall("lm_spi_write")
+		.withPointerParameter("dev", dev)
+		.withMemoryBufferParameter("data", (const uint8_t *)data, data_len)
 		.returnIntValue();
 }
 
-static int writeread(struct spi *self, const void *txdata, size_t txdata_len,
-		void *rxbuf, size_t rxbuf_len) {
-	ByteArray txPtrLen = {
-		txdata, txdata_len
-	};
-	return mock().actualCall("spi_writeread")
-		.withParameter("self", self)
-		.withParameterOfType("ByteArray", "ptrlen", &txPtrLen)
+int lm_spi_read(struct lm_spi_device *dev, void *buf, size_t rx_len) {
+	return mock().actualCall("lm_spi_read")
+		.withPointerParameter("dev", dev)
+		.withOutputParameter("buf", buf)
+		.returnIntValue();
+}
+
+int lm_spi_writeread(struct lm_spi_device *dev,
+		const void *txdata, size_t txdata_len,
+		void *rxbuf, size_t rx_len) {
+	return mock().actualCall("lm_spi_writeread")
+		.withPointerParameter("dev", dev)
+		.withMemoryBufferParameter("txdata", (const uint8_t *)txdata, txdata_len)
 		.withOutputParameter("rxbuf", rxbuf)
-		.withParameter("rxbuf_len", rxbuf_len)
 		.returnIntValue();
-}
-
-struct spi *spi_create(uint8_t channel) {
-	(void)channel;
-
-	static struct spi spi = {
-		.api = {
-			.enable = enable_spi,
-			.disable = disable_spi,
-			.write = write_spi,
-			.read = read_spi,
-			.writeread = writeread,
-		},
-	};
-
-	return &spi;
-}
-
-void spi_delete(struct spi *self) {
-	mock().actualCall(__func__).withParameter("self", self);
 }
