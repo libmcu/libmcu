1f8ae9a573dc12f272a0d9061aa6a52c246ba794
Kyunghwan Kwon <k@libmcu.org>
2025-10-31 23:47:00 +0900

    feat(wdt): add stub implementations for new WDT functions (#315)

--- tests/stubs/wdt.cpp
+++ tests/stubs/wdt.cpp
@@ -6,12 +6,53 @@ struct wdt {
 
 struct wdt *wdt_new(const char *name, const uint32_t period_ms,
 		wdt_timeout_cb_t cb, void *cb_ctx) {
+	(void)name;
+	(void)period_ms;
+	(void)cb;
+	(void)cb_ctx;
 	static struct wdt wdt;
 	return &wdt;
 }
 
 void wdt_delete(struct wdt *self) {
 	(void)self;
+}
+
+int wdt_start(void) {
+	return 0;
+}
+
+void wdt_stop(void) {
+}
+
+int wdt_step(uint32_t *next_deadline_ms) {
+	(void)next_deadline_ms;
+	return 0;
+}
+
+uint32_t wdt_get_time_since_last_feed(const struct wdt *self) {
+	(void)self;
+	return 0;
+}
+
+uint32_t wdt_get_period(const struct wdt *self) {
+	(void)self;
+	return 0;
+}
+
+void wdt_foreach(wdt_foreach_cb_t cb, void *cb_ctx) {
+	(void)cb;
+	(void)cb_ctx;
+}
+
+const char *wdt_name(const struct wdt *self) {
+	(void)self;
+	return "stub_wdt";
+}
+
+bool wdt_is_enabled(const struct wdt *self) {
+	(void)self;
+	return false;
 }
 
 int wdt_enable(struct wdt *self) {
@@ -35,10 +76,11 @@ int wdt_feed(struct wdt *self) {
 	return 0;
 }
 
-int wdt_init(wdt_periodic_cb_t cb, void *cb_ctx)
+int wdt_init(wdt_periodic_cb_t cb, void *cb_ctx, bool threaded)
 {
 	(void)cb;
 	(void)cb_ctx;
+	(void)threaded;
 	return 0;
 }
 
